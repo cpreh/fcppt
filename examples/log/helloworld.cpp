734506b3f3ef779dc017b6f22ea52b60a9d822e7
Philipp Reh <sefi@s-e-f-i.de>
2010-01-24 13:36:06 +0100

    Moved test to examples.

--- examples/log/helloworld.cpp
+++ examples/log/helloworld.cpp
@@ -18,66 +18,34 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 
-#ifndef FCPPT_LOG_PARAMETERS_ROOT_HPP_INCLUDED
-#define FCPPT_LOG_PARAMETERS_ROOT_HPP_INCLUDED
-
-#include <fcppt/log/parameters/root_fwd.hpp>
-#include <fcppt/log/parameters/all.hpp>
+#include <fcppt/log/object.hpp>
+#include <fcppt/log/headers.hpp>
+#include <fcppt/log/parameters/root.hpp>
 #include <fcppt/log/level.hpp>
-#include <fcppt/log/context_fwd.hpp>
-#include <fcppt/io/ostream.hpp>
-#include <fcppt/string.hpp>
-#include <fcppt/symbol.hpp>
+#include <fcppt/io/cout.hpp>
+#include <fcppt/text.hpp>
 
-namespace fcppt
+int main()
 {
-namespace log
-{
-namespace parameters
-{
-
-class root {
-public:
-	FCPPT_SYMBOL explicit root(
-		io::ostream &
+	fcppt::log::object logger_(
+		fcppt::log::parameters::root(
+			fcppt::io::cout
+		)
+		.prefix(
+			FCPPT_TEXT("mylogger")
+		)
+		.enabled(
+			true
+		)
+		.level(
+			fcppt::log::level::debug
+		)
+		.create()
 	);
 
-	FCPPT_SYMBOL root
-	prefix(
-		string const &
+	FCPPT_LOG_DEBUG(
+		logger_,
+		fcppt::log::_
+			<< FCPPT_TEXT("Hello World")
 	);
-
-	FCPPT_SYMBOL root
-	enabled(
-		bool
-	);
-
-	FCPPT_SYMBOL root
-	level(
-		log::level::type
-	);
-
-	FCPPT_SYMBOL root
-	context(
-		log::context &
-	);
-
-	FCPPT_SYMBOL all const
-	create() const;
-private:
-	io::ostream &sink_;
-
-	string prefix_;
-
-	bool enabled_;
-
-	log::level::type level_;
-
-	log::context *context_;
-};
-
 }
-}
-}
-
-#endif
