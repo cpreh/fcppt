36db3c5a13ca32d0cb52ee33c1d0a6e02d86f82d
Philipp Reh <sefi@s-e-f-i.de>
2010-01-30 21:44:51 +0100

    Don't apply license headers to third party stuff.

--- util/update_license.sh
+++ util/update_license.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 for folder in examples include src 'test' ; do
-	find ${folder} -name '*.?pp' -exec update_license '{}' util/license_template.hpp \;
+	find ${folder} -not -name facet.cpp -not -name ratio.hpp -name '*.?pp' -exec update_license '{}' util/license_template.hpp \;
 done
