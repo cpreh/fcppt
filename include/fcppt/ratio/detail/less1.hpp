e19e55b15138de8b62cd30deb8ad020f6a10ea52
Philipp Reh <sefi@s-e-f-i.de>
2010-01-31 14:23:36 +0100

    Added license headers.

--- include/fcppt/ratio/detail/less1.hpp
+++ include/fcppt/ratio/detail/less1.hpp
@@ -1,3 +1,9 @@
+//          Copyright Carl Philipp Reh 2009 - 2010.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
 #ifndef FCPPT_RATIO_DETAIL_LESS1_HPP_INCLUDED
 #define FCPPT_RATIO_DETAIL_LESS1_HPP_INCLUDED
 
