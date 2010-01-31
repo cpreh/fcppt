56e4bd771f56ac15a84e33fde5f8a3a235714e94
Philipp Reh <sefi@s-e-f-i.de>
2010-01-31 20:00:58 +0100

    Don't exclude ratio.hpp for license headers.

--- util/update_license.sh
+++ util/update_license.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 for folder in examples include src 'test' ; do
-	find ${folder} -not -name facet.cpp -not -name ratio.hpp -name '*.?pp' -exec update_license '{}' util/license_template.hpp \;
+	find ${folder} -not -name facet.cpp -name '*.?pp' -exec update_license '{}' util/license_template.hpp \;
 done
