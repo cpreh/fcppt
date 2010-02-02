ba9dd6c04d09986d4dc5beb2a9ded91a2272bae1
phillemann <pmidden@gmx.net>
2010-02-02 23:33:49 +0100

    added better assertions

--- include/fcppt/assert.hpp
+++ include/fcppt/assert.hpp
@@ -7,20 +7,21 @@
 #ifndef FCPPT_ASSERT_HPP_INCLUDED
 #define FCPPT_ASSERT_HPP_INCLUDED
 
-#include <fcppt/detail/process_assert.hpp>
 #include <fcppt/preprocessor/stringize.hpp>
 #include <fcppt/preprocessor/file.hpp>
+#include <fcppt/preprocessor/function.hpp>
+#include <fcppt/assertion_failed.hpp>
 
 /**
- * @brief Runtime assert macro that throws fcppt::exception
+ * @brief Runtime assert macro that throws fcppt::assertion_failed
 */
 #define FCPPT_ASSERT(cond)\
 if (!(cond))\
-	fcppt::detail::process_assert(\
+	throw fcppt::assertion_failed(\
 		FCPPT_PP_FILE,\
-		FCPPT_PP_STRINGIZE(__LINE__),\
+		__LINE__,\
+		FCPPT_PP_FUNCTION,\
 		FCPPT_PP_STRINGIZE(cond),\
-		FCPPT_PP_STRINGIZE("")\
-	);
+		FCPPT_PP_STRINGIZE(""));
 
 #endif
---
+++ include/fcppt/assertion_failed.hpp
@@ -0,0 +1,52 @@
+//          Copyright Carl Philipp Reh 2009 - 2010.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+#ifndef FCPPT_ASSERTION_FAILED_HPP_INCLUDED
+#define FCPPT_ASSERTION_FAILED_HPP_INCLUDED
+
+#include <fcppt/exception.hpp>
+#include <fcppt/string.hpp>
+#include <fcppt/class_symbol.hpp>
+#include <fcppt/symbol.hpp>
+
+namespace fcppt
+{
+class FCPPT_CLASS_SYMBOL assertion_failed
+:
+	public exception
+{
+public:
+	FCPPT_SYMBOL assertion_failed(
+		fcppt::string const &file,
+		int line,
+		fcppt::string const &function,
+		fcppt::string const &condition,
+		fcppt::string const &message);
+	
+	FCPPT_SYMBOL fcppt::string const &
+	file() const;
+
+	FCPPT_SYMBOL int	
+	line() const;
+
+	FCPPT_SYMBOL fcppt::string const &
+	function() const;
+
+	FCPPT_SYMBOL fcppt::string const &
+	condition() const;
+
+	FCPPT_SYMBOL fcppt::string const &
+	message() const;
+
+	~assertion_failed() throw();
+private:
+	fcppt::string file_;
+	int line_;
+	fcppt::string function_;
+	fcppt::string condition_;
+	fcppt::string message_;
+};
+}
+
+#endif
