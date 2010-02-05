961e32cace84f122c51fe90265d6d771a7c7a07a
Philipp Reh <sefi@s-e-f-i.de>
2010-02-05 13:13:36 +0100

    Added a truncation_check_cast for sleep.

--- src/time/sleep.cpp
+++ src/time/sleep.cpp
@@ -7,12 +7,13 @@
 #include <fcppt/time/sleep.hpp>
 #include <fcppt/time/sleep_interrupted.hpp>
 #include <fcppt/chrono/duration_impl.hpp>
+#include <fcppt/truncation_check_cast.hpp>
 #include <fcppt/config.h>
 #if defined(FCPPT_WINDOWS_PLATFORM)
-#include <fcppt/truncation_check_cast.hpp>
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #elif defined(FCPPT_POSIX_PLATFORM)
+#include <ctime>
 #include <time.h>
 #else
 #error "Don't know what to include for sleep!"
@@ -39,8 +40,16 @@ fcppt::time::sleep(
 #elif defined(FCPPT_POSIX_PLATFORM)
 	timespec const req =
 	{
-		duration_.count() / sleep_duration::period::den,
-		duration_.count() % sleep_duration::period::den
+		truncation_check_cast<
+			std::time_t
+		>(
+			duration_.count() / sleep_duration::period::den
+		),
+		truncation_check_cast<
+			long
+		>(
+			duration_.count() % sleep_duration::period::den
+		)
 	};
 
 	timespec rem;
