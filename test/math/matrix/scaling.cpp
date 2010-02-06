b168dd19d1550fd47a0a279a555d07dacf72ad19
Philipp Reh <sefi@s-e-f-i.de>
2010-02-06 15:40:43 +0100

    More work on the math matrix scaling test.

--- test/math/matrix/scaling.cpp
+++ test/math/matrix/scaling.cpp
@@ -6,6 +6,8 @@
 
 #include <fcppt/math/matrix/matrix.hpp>
 #include <fcppt/math/matrix/scaling.hpp>
+#include <fcppt/math/vector/vector.hpp>
+#include <fcppt/math/vector/static.hpp>
 #include <boost/test/unit_test.hpp>
 
 BOOST_AUTO_TEST_CASE(math_matrix_scaling)
@@ -34,4 +36,28 @@ BOOST_AUTO_TEST_CASE(math_matrix_scaling)
 			0.f, 0.f, 0.f, 1.f
 		)
 	);
+
+	typedef fcppt::math::vector::static_<
+		float,
+		4
+	>::type vector_type;
+
+	vector_type const vec_(
+		1.f,
+		2.f,
+		3.f,
+		1.f
+	);
+
+	BOOST_REQUIRE(
+		trans_ * vec_
+		==
+		vector_type(
+			5.f,
+			6.f,
+			12.f,
+			1.f
+		)
+	);
+
 }
