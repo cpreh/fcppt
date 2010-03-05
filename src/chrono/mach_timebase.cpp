68fb5fd5d66c006ca098801226c2762543574040
Philipp Reh <sefi@s-e-f-i.de>
2010-03-05 20:52:55 +0100

    Added mac stuff.

---
+++ src/chrono/mach_timebase.cpp
@@ -0,0 +1,38 @@
+//          Copyright Carl Philipp Reh 2009 - 2010.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <fcppt/config.h>
+#ifdef FCPPT_HAVE_MACH_TIME
+#include "mach_timebase.hpp"
+#include <fcppt/function_once.hpp>
+
+namespace
+{
+
+struct mach_timebase_info ratio_;
+
+void
+init()
+{
+	FCPPT_FUNCTION_ONCE
+
+	// TODO: what can this return?
+	mach_timebase_info(
+		&ratio_
+	);
+}
+
+}
+
+struct mach_timebase_info const
+fcppt::chrono::mach_timebase()
+{
+	init();
+
+	return ratio_;
+}
+
+#endif
