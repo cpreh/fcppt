43a7ac8739444ead085025c40cf692739e5c2b69
Philipp Reh <sefi@s-e-f-i.de>
2010-03-06 15:22:55 +0100

    Added vector example.

---
+++ examples/math/vector.cpp
@@ -0,0 +1,12 @@
+#include <fcppt/math/vector/vector.hpp>
+#include <fcppt/math/vector/static.hpp>
+
+int main()
+{
+	typedef fcppt::math::vector::static_<
+		float,
+		3
+	>::type float3_vec;
+
+	
+}
