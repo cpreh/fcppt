81c8f0a6b9bfa7a29f6a8119e9ed7f3cdff2291c
Philipp Reh <sefi@s-e-f-i.de>
2010-03-06 11:58:19 +0100

    Added another math test.

---
+++ test/math/util.cpp
@@ -0,0 +1,44 @@
+//          Copyright Carl Philipp Reh 2009 - 2010.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <fcppt/math/is_power_of_2.hpp>
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(math_almost_zero)
+{
+	BOOST_REQURE(
+		fcppt::math::almost_zero(
+			0
+		)
+	);
+
+	BOOST_REQUIRE(
+		fcppt::math::almost_zero(
+			0.0
+		)
+	);
+}
+
+BOOST_AUTO_TEST_CASE(math_is_power_of_2)
+{
+	BOOST_REQUIRE(
+		fcppt::math::is_power_of_2(
+			1024u
+		)
+	);
+
+	BOOST_REQUIRE(
+		!fcppt::math::is_power_of_2(
+			0u
+		)
+	);
+
+	BOOST_REQUIRE(
+		!fcppt::math::is_power_of_2(
+			3u
+		)
+	);
+}
