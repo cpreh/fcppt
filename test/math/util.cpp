827ca8073d489a7408eb74bbd37c5ab9cfebe0ae
Philipp Reh <sefi@s-e-f-i.de>
2010-03-06 13:17:25 +0100

    Removed negative.hpp.

--- test/math/util.cpp
+++ test/math/util.cpp
@@ -4,8 +4,9 @@
 //          http://www.boost.org/LICENSE_1_0.txt)
 
 
-#include <fcppt/math/is_power_of_2.hpp>
 #include <fcppt/math/almost_zero.hpp>
+#include <fcppt/math/diff.hpp>
+#include <fcppt/math/is_power_of_2.hpp>
 #include <boost/test/unit_test.hpp>
 
 BOOST_AUTO_TEST_CASE(math_almost_zero)
@@ -41,5 +42,24 @@ BOOST_AUTO_TEST_CASE(math_is_power_of_2)
 		!fcppt::math::is_power_of_2(
 			3u
 		)
+	);
+}
+
+BOOST_AUTO_TEST_CASE(math_diff)
+{
+	BOOST_CHECK(
+		fcppt::math::diff(
+			10u,
+			20u
+		)
+		== 10u
+	);
+
+	BOOST_CHECK(
+		fcppt::math::diff(
+			-1,
+			1
+		)
+		== 2
 	);
 }
