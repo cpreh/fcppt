eee60a15d292596c2982b326d0c06f5ad9311e34
Philipp Reh <sefi@s-e-f-i.de>
2010-03-09 18:14:47 +0100

    Added missing license headers.

--- examples/math/vector/convert.cpp
+++ examples/math/vector/convert.cpp
@@ -1,4 +1,8 @@
-//[vectorconvert
+//          Copyright Carl Philipp Reh 2009 - 2010.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
 
 #include <fcppt/math/vector/vector.hpp>
 #include <fcppt/math/vector/static.hpp>
