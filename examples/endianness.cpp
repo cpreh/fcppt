30594ada33b7530f4844827dbcd6bb3a4ddb0e59
Philipp Reh <sefi@s-e-f-i.de>
2010-03-09 21:47:17 +0100

    Updated license headers.

--- examples/endianness.cpp
+++ examples/endianness.cpp
@@ -1,4 +1,9 @@
-//[endianness
+//          Copyright Carl Philipp Reh 2009 - 2010.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
 #include <fcppt/endianness/is_big_endian.hpp>
 #include <fcppt/endianness/is_little_endian.hpp>
 #include <fcppt/endianness/to_little_endian.hpp>
