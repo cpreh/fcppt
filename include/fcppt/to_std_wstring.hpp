2149d128e43fadcaf676d20d1703659f14df2699
Philipp Reh <sefi@s-e-f-i.de>
2010-03-09 20:49:37 +0100

    Added from/to_std_wstring.

---
+++ include/fcppt/to_std_wstring.hpp
@@ -0,0 +1,24 @@
+//          Copyright Carl Philipp Reh 2009 - 2010.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef FCPPT_TO_STD_WSTRING_HPP_INCLUDED
+#define FCPPT_TO_STD_WSTRING_HPP_INCLUDED
+
+#include <fcppt/string.hpp>
+#include <fcppt/symbol.hpp>
+#include <string>
+
+namespace fcppt
+{
+
+FCPPT_SYMBOL std::wstring const
+to_std_wstring(
+	string const &
+);
+
+}
+
+#endif
---
+++ src/to_std_wstring.cpp
@@ -0,0 +1,23 @@
+//          Copyright Carl Philipp Reh 2009 - 2010.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <fcppt/to_std_wstring.hpp>
+#include <fcppt/config.hpp>
+#ifdef FCPPT_NARROW_STRING
+#include <fcppt/widen.hpp>
+#endif
+
+std::wstring const
+fcppt::to_std_wstring(
+	string const &input
+)
+{
+#ifdef FCPPT_NARROW_STRING
+	return widen(input);
+#else
+	return input;
+#endif
+}
