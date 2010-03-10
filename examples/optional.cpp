fea280e2f86dc0f3589537c5c4c8dffb744c0ea5
Philipp Reh <sefi@s-e-f-i.de>
2010-03-10 18:17:19 +0100

    Added variant documentation.

--- examples/optional.cpp
+++ examples/optional.cpp
@@ -4,6 +4,7 @@
 //          http://www.boost.org/LICENSE_1_0.txt)
 
 
+//[optional
 #include <fcppt/io/cout.hpp>
 #include <fcppt/optional_impl.hpp>
 #include <fcppt/string.hpp>
@@ -11,14 +12,17 @@
 
 int main()
 {
+	// typedef an optional string
 	typedef fcppt::optional<
 		fcppt::string
 	> optional_string;
 
+	// set the optional to "test"
 	optional_string opt1(
 		FCPPT_TEXT("test")
 	);
 
+	// copy opt1 by value
 	optional_string opt2(
 		opt1
 	);
@@ -30,6 +34,8 @@ int main()
 		<< FCPPT_TEXT('\n');
 
 	opt2 = FCPPT_TEXT("test2");
+
+	// assign opt1 by value
 	opt1 = opt2;
 
 	fcppt::io::cout
@@ -41,8 +47,10 @@ int main()
 	if(opt1)
 		fcppt::io::cout << FCPPT_TEXT("opt1 is set\n");
 
+	// destroy opt1's value
 	opt1.reset();
 
 	if(!opt1)
 		fcppt::io::cout << FCPPT_TEXT("opt1 is not set\n");
 }
+//]
