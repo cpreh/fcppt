563363e87ade02ee82ba6abcf541011f4809b825
phillemann <pmidden@gmx.net>
2010-05-12 00:33:18 +0200

    added new quaternion function, renamed test

---
+++ include/fcppt/math/quaternion/to_angle_and_axis.hpp
@@ -0,0 +1,38 @@
+#ifndef FCPPT_MATH_QUATERNION_TO_ANGLE_AND_AXIS_HPP_INCLUDED
+#define FCPPT_MATH_QUATERNION_TO_ANGLE_AND_AXIS_HPP_INCLUDED
+
+#include <boost/math/quaternion.hpp>
+#include <fcppt/math/vector/static.hpp>
+#include <fcppt/math/vector/basic_impl.hpp>
+#include <cmath>
+
+namespace fcppt
+{
+namespace math
+{
+namespace quaternion
+{
+template<typename T>
+void
+to_angle_and_axis(
+	boost::math::quaternion<T> const &q,
+	T &angle,
+	typename fcppt::math::vector::static_<T,3>::type &axis)
+{
+	// NOTE: Can we express this with quaternion functions instead of the manual sqrt?
+	T const scale = 
+		std::sqrt(
+			q.R_component_2() * q.R_component_2() + 
+			q.R_component_3() * q.R_component_3() + 
+			q.R_component_4() * q.R_component_4());
+
+	axis.x() = q.R_component_2() / scale;
+	axis.y() = q.R_component_3() / scale;
+	axis.z() = q.R_component_4() / scale;
+	angle = std::acos(q.R_component_2()) * static_cast<T>(2.0);
+}
+}
+}
+}
+
+#endif
