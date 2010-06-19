4f9457cb1396e1efd09e856ba4e94b6ce28fb21f
Philipp Reh <sefi@s-e-f-i.de>
2010-06-19 22:33:03 +0200

    Added std_time function.

---
+++ include/fcppt/time/std_time.hpp
@@ -0,0 +1,30 @@
+//          Copyright Carl Philipp Reh 2009 - 2010.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef FCPPT_TIME_STD_TIME_HPP_INCLUDED
+#define FCPPT_TIME_STD_TIME_HPP_INCLUDED
+
+#include <fcppt/symbol.hpp>
+#include <ctime>
+
+namespace fcppt
+{
+namespace time
+{
+
+/// Returns the seconds since the epoch (1.1.1970 00:00)
+/**
+ * Calls std::time(0)
+ * @throws time::exception if an error occurs
+*/
+FCPPT_SYMBOL
+std::time_t
+std_time();
+
+}
+}
+
+#endif
---
+++ src/time/std_time.cpp
@@ -0,0 +1,27 @@
+//          Copyright Carl Philipp Reh 2009 - 2010.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <fcppt/time/std_time.hpp>
+#include <fcppt/time/exception.hpp>
+#include <fcppt/text.hpp>
+#include <ctime>
+
+std::time_t
+fcppt::time::std_time()
+{
+	std::time_t const ret(
+		std::time(0)
+	);
+
+	if(
+		ret == static_cast<std::time_t>(-1)
+	)
+		throw time::exception(
+			FCPPT_TEXT("std_time() failed!")
+		);
+	
+	return ret;
+}
