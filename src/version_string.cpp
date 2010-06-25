485198db7736ed9a2c265dd2df41581a0b6df9e8
Philipp Reh <sefi@s-e-f-i.de>
2010-06-25 20:44:01 +0200

    Added more version stuff and changed the version to 0.2.

---
+++ src/version_string.cpp
@@ -0,0 +1,34 @@
+//          Copyright Carl Philipp Reh 2009 - 2010.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <fcppt/version_string.hpp>
+#include <fcppt/lexical_cast.hpp>
+#include <fcppt/major_version.hpp>
+#include <fcppt/minor_version.hpp>
+#include <fcppt/micro_version.hpp>
+
+fcppt::string const
+fcppt::version_string()
+{
+	return
+		fcppt::lexical_cast<
+			fcppt::string
+		>(
+			fcppt::major_version
+		)
+		+ FCPPT_TEXT('.')
+		+ fcppt::lexical_cast<
+			fcppt::string
+		>(
+			fcppt::minor_version
+		)
+		+ FCPPT_TEXT('.')
+		+ fcppt::lexical_cast<
+			fcppt::string
+		>(
+			fcppt::micro_version
+		);
+}
