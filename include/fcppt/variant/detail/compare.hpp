556c390829816e85fe2245a6b8c86be13d483c49
Philipp Reh <sefi@s-e-f-i.de>
2010-08-13 11:37:13 +0200

    Added operator== and != for variant.

---
+++ include/fcppt/variant/detail/compare.hpp
@@ -0,0 +1,69 @@
+//          Copyright Carl Philipp Reh 2009 - 2010.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef FCPPT_VARIANT_DETAIL_COMPARE_HPP_INCLUDED
+#define FCPPT_VARIANT_DETAIL_COMPARE_HPP_INCLUDED
+
+#include <fcppt/variant/object_decl.hpp>
+#include <fcppt/variant/holds_type.hpp>
+
+namespace fcppt
+{
+namespace variant
+{
+namespace detail
+{
+
+template<
+	typename Types
+>
+class compare
+{
+public:
+	typedef variant::object<
+		Types
+	> variant_type;
+
+	explicit compare(
+		variant_type const &_left
+	)
+	:
+		left_(_left)
+	{}
+
+	typedef bool result_type;
+
+	template<
+		typename OtherType
+	>
+	result_type
+	operator()(
+		OtherType const &_right
+	) const
+	{
+		return
+			!variant::holds_type<
+				OtherType
+			>(
+				left_
+			)
+			?
+				false
+			:
+				left_. template get<
+					OtherType
+				>()
+				== _right;
+	}
+private:
+	variant_type const &left_;
+};
+
+}
+}
+}
+
+#endif
---
+++ test/variant/equal.cpp
@@ -0,0 +1,62 @@
+//          Copyright Carl Philipp Reh 2009 - 2010.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <fcppt/variant/equal.hpp>
+#include <fcppt/variant/not_equal.hpp>
+#include <fcppt/variant/object.hpp>
+#include <boost/mpl/vector/vector10.hpp>
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(variant_equal)
+{
+	typedef fcppt::variant::object<
+		boost::mpl::vector3<
+			bool,
+			int,
+			std::string
+		>
+	> variant;
+
+	std::string const string(
+		"hello world"
+	);
+
+	variant const v1(
+		string
+	);
+
+	variant const v2(
+		string
+	);
+
+	variant const empty1;
+
+	variant const empty2;
+
+	BOOST_REQUIRE(
+		v1 == v2
+	);
+
+	BOOST_REQUIRE(
+		empty1 == empty2
+	);
+
+	BOOST_REQUIRE(
+		empty1 != v1
+	);
+
+	BOOST_REQUIRE(
+		v1 != empty1
+	);
+
+	variant const v3(
+		42
+	);
+
+	BOOST_REQUIRE(
+		v1 != v3
+	);
+}
