14050ef9db96bcc6cd4d1a5ea0363e80c49d2901
Philipp Reh <sefi@s-e-f-i.de>
2010-09-17 17:22:32 +0200

    Added path_to_string.

---
+++ include/fcppt/filesystem/path_to_string.hpp
@@ -0,0 +1,28 @@
+//          Copyright Carl Philipp Reh 2009 - 2010.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef FCPPT_FILESYSTEM_PATH_TO_STRING_HPP_INCLUDED
+#define FCPPT_FILESYSTEM_PATH_TO_STRING_HPP_INCLUDED
+
+#include <fcppt/filesystem/path.hpp>
+#include <fcppt/string.hpp>
+#include <fcppt/symbol.hpp>
+
+namespace fcppt
+{
+namespace filesystem
+{
+
+FCPPT_SYMBOL
+fcppt::string const
+path_to_string(
+	fcppt::filesystem::path const &
+);
+
+}
+}
+
+#endif
---
+++ src/filesystem/path_to_string.cpp
@@ -0,0 +1,18 @@
+//          Copyright Carl Philipp Reh 2009 - 2010.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <fcppt/filesystem/path_to_string.hpp>
+
+fcppt::string const
+fcppt::filesystem::path_to_string(
+	fcppt::filesystem::path const &_path
+)
+{
+	return
+		_path.string<
+			fcppt::string
+		>();
+}
