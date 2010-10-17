66f37b36f0275acf2444009272c6626a9850561f
phillemann <pmidden@gmx.net>
2010-10-17 22:17:44 +0200

    Added type traits stuff
    - generate_has_member_function generates a structure which tests for a
      given member function
    - generate_has_type generates a structure which tests if the structure
      has a specific type (like ::iterator)
    - is_iterable determines if a type has the ::iterator or
      ::const_iterator type (depending on the constness of the type). This
      is called is_iterable on purpose, since it might be replaced by a
      more powerful test which includes testing for begin/end
    - Those 3 functions are tested in test/type_traits

---
+++ include/fcppt/type_traits/has_const_iterator.hpp
@@ -0,0 +1,15 @@
+#ifndef FCPPT_TYPE_TRAITS_HAS_CONST_ITERATOR_HPP_INCLUDED
+#define FCPPT_TYPE_TRAITS_HAS_CONST_ITERATOR_HPP_INCLUDED
+
+#include <fcppt/type_traits/generate_has_type.hpp>
+
+namespace fcppt
+{
+namespace type_traits
+{
+FCPPT_TYPE_TRAITS_GENERATE_HAS_TYPE(
+	const_iterator);
+}
+}
+
+#endif
