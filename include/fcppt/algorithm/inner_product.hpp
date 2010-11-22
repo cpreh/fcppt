49874515e3039fdd75174666f61373e31613cb81
phillemann <pmidden@gmx.net>
2010-11-22 22:33:00 +0100

    Added 3-parameter inner_product

--- include/fcppt/algorithm/inner_product.hpp
+++ include/fcppt/algorithm/inner_product.hpp
@@ -29,6 +29,27 @@ inner_product(
 
 	return result;
 }
+
+template
+<
+	typename Range1,
+	typename Range2,
+	typename ValueType
+>
+ValueType const
+inner_product(
+	Range1 const &r1,
+	Range2 const &r2,
+	ValueType result)
+{
+	typename Range1::const_iterator r1i = r1.begin();
+	typename Range2::const_iterator r2i = r2.begin();
+
+	for (;r1i != r1.end(); ++r1i, ++r2i)
+		result = result + (*r1i) * (*r2i);
+
+	return result;
+}
 }
 }
 
