24da512b4501981117a5668ed1c9ad53312d8ced
Philipp Reh <sefi@s-e-f-i.de>
2010-11-25 20:10:55 +0100

    Added container::array.

---
+++ include/fcppt/container/out_of_range.hpp
@@ -0,0 +1,31 @@
+//          Copyright Carl Philipp Reh 2009 - 2010.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef FCPPT_CONTAINER_OUT_OF_RANGE_HPP_INCLUDED
+#define FCPPT_CONTAINER_OUT_OF_RANGE_HPP_INCLUDED
+
+#include <fcppt/container/exception.hpp>
+#include <fcppt/string.hpp>
+
+namespace fcppt
+{
+namespace container
+{
+
+class out_of_range
+:
+	public container::exception
+{
+public:
+	out_of_range();
+};
+
+}
+}
+
+#include <fcppt/container/impl/out_of_range.hpp>
+
+#endif
