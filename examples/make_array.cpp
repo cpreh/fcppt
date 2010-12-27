8b71d75b90a4b4909657e974e1a25d2136fbc87e
Philipp Reh <sefi@s-e-f-i.de>
2010-12-27 01:27:09 +0100

    Fixed license headers.

--- examples/make_array.cpp
+++ examples/make_array.cpp
@@ -3,6 +3,7 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
+
 //[make_array
 #include <fcppt/assign/make_array.hpp>
 #include <fcppt/container/array.hpp>
