069a677fe1e1a9777f033803cbcd5c9c1ea21e4d
Philipp Reh <sefi@s-e-f-i.de>
2011-01-02 15:14:37 +0100

    Updated license years.

--- include/fcppt/ratio/centi.hpp
+++ include/fcppt/ratio/centi.hpp
@@ -1,6 +1,6 @@
 //  Copyright 2008 Howard Hinnant
 //  Copyright 2008 Beman Dawes
-//  Copyright 2009-2010 Carl Philipp Reh
+//  Copyright 2009-2011 Carl Philipp Reh
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
