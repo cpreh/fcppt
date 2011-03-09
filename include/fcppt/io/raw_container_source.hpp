59d9dd05d0de4ccd876b30415a7049b738929f12
Philipp Reh <sefi@s-e-f-i.de>
2011-03-09 15:04:15 +0100

    Updated license headers.

--- include/fcppt/io/raw_container_source.hpp
+++ include/fcppt/io/raw_container_source.hpp
@@ -1,3 +1,9 @@
+//          Copyright Carl Philipp Reh 2009 - 2011.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
 #ifndef FCPPT_IO_RAW_CONTAINER_SOURCE_HPP_INCLUDED
 #define FCPPT_IO_RAW_CONTAINER_SOURCE_HPP_INCLUDED
 
