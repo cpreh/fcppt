a2f8a0583ecf107231e2ad4bf73dffe20244f6da
phillemann <pmidden@gmx.net>
2011-03-11 18:33:36 +0100

    Added vector::hypersphere_to_cartesian

---
+++ include/fcppt/math/vector/hypersphere_to_cartesian.hpp
@@ -0,0 +1,71 @@
+#ifndef FCPPT_MATH_VECTOR_HYPERSPHERE_TO_CARTESIAN_HPP_INCLUDED
+#define FCPPT_MATH_VECTOR_HYPERSPHERE_TO_CARTESIAN_HPP_INCLUDED
+
+#include <fcppt/math/vector/basic.hpp>
+#include <fcppt/math/vector/static.hpp>
+#include <cmath>
+
+namespace fcppt
+{
+namespace math
+{
+namespace vector
+{
+/// Calculate the n dimensional polar coordinates, also called "hyperspherical coordinates"
+/**
+	Formula taken from: 
+	http://en.wikipedia.org/wiki/Hypersphere#Hyperspherical_coordinates
+
+	Given an (n-1)-dimensional angle vector this returns an
+	n-dimensional unit vector in n-dimensional cartesian coordinates. If
+	you want a scaled version of the result, do it yourself using
+	operator*(T,vector).
+
+	@param angles The n-1 dimensional angle vector
+ */
+template<typename T,typename N,typename S>
+typename
+static_<T,N::value+1>::type const
+hypersphere_to_cartesian(
+	basic<T,N,S> const &angles)
+{
+	typedef typename
+	static_<T,N::value+1>::type 
+	result_type;
+
+	typedef typename
+	result_type::size_type
+	size_type;
+
+	typedef typename
+	result_type::value_type
+	value_type;
+
+	result_type result;
+	for(size_type i = 0; i < result.size(); ++i)
+	{
+		// Hehe..."sins"
+		value_type sins = 
+			static_cast<value_type>(
+				1);
+		for(size_type j = 0; j < i; ++j)
+			sins *= 
+				std::sin(
+					angles[j]);
+		result[i] = 
+			sins * 
+			(
+			i >= angles.size()
+			?
+				static_cast<value_type>(1)
+			:
+				std::cos(
+					angles[i]));
+	}
+	return result;
+}
+}
+}
+}
+
+#endif
--- test/math/vector.cpp
+++ test/math/vector.cpp
@@ -8,30 +8,44 @@
 #include <fcppt/math/compare.hpp>
 #include <fcppt/math/pi.hpp>
 #include <fcppt/math/size_type.hpp>
+#include <fcppt/math/range_compare.hpp>
+#include <fcppt/math/vector/hypersphere_to_cartesian.hpp>
+#include <fcppt/math/vector/output.hpp>
 #include <boost/mpl/integral_c.hpp>
 #include <boost/test/unit_test.hpp>
 #include <algorithm>
+#include <iostream>
 
 namespace
 {
+typedef 
+float 
+real;
 
-typedef fcppt::math::vector::static_<
-	unsigned,
-	2
->::type ui2_vec;
+typedef 
+fcppt::math::vector::static_<unsigned,2>::type 
+uivector2;
 
-typedef float float_type;
+typedef
+fcppt::math::vector::static_<real,1>::type
+fvector1;
 
-typedef fcppt::math::vector::static_<
-	float_type,
-	2
->::type f2_vec;
+typedef
+fcppt::math::vector::static_<real,2>::type
+fvector2;
 
+typedef
+fcppt::math::vector::static_<real,3>::type
+fvector3;
+
+real const epsilon = 
+	static_cast<real>(
+		0.001);
 }
 
 BOOST_AUTO_TEST_CASE(vector_compare)
 {
-	ui2_vec const
+	uivector2 const
 		vec1(1, 2),
 		vec2(1, 2),
 		vec3(2, 2);
@@ -47,7 +61,7 @@ BOOST_AUTO_TEST_CASE(vector_compare)
 
 BOOST_AUTO_TEST_CASE(vector_angle_between)
 {
-	f2_vec const
+	fvector2 const
 		vec1(1.f, 0.f),
 		vec2(0.f, 1.f);
 	
@@ -57,33 +71,33 @@ BOOST_AUTO_TEST_CASE(vector_angle_between)
 				vec1,
 				vec2
 			),
-			fcppt::math::pi<float_type>() / 2.f
+			fcppt::math::pi<real>() / 2.f
 		)
 	);
 }
 
 BOOST_AUTO_TEST_CASE(vector_angle_between_cast)
 {
-	ui2_vec const
+	uivector2 const
 		vec1(1, 0),
 		vec2(0, 1);
 	
 	BOOST_CHECK(
 		fcppt::math::compare(
 			fcppt::math::vector::angle_between_cast<
-				float_type
+				real
 			>(
 				vec1,
 				vec2
 			),
-			fcppt::math::pi<float_type>() / 2.f
+			fcppt::math::pi<real>() / 2.f
 		)
 	);
 }
 
 BOOST_AUTO_TEST_CASE(vector_signed_angle)
 {
-	f2_vec const
+	fvector2 const
 		vec1(2.f, 1.f),
 		vec2(2.f, 2.f);
 	
@@ -93,38 +107,38 @@ BOOST_AUTO_TEST_CASE(vector_signed_angle)
 				vec1,
 				vec2
 			),
-			fcppt::math::pi<float_type>() / 2.f
+			fcppt::math::pi<real>() / 2.f
 		)
 	);
 }
 
 BOOST_AUTO_TEST_CASE(vector_signed_angle_cast)
 {
-	ui2_vec const
+	uivector2 const
 		vec1(2, 1),
 		vec2(2, 2);
 	
 	BOOST_CHECK(
 		fcppt::math::compare(
 			fcppt::math::vector::signed_angle_cast<
-				float_type
+				real
 			>(
 				vec1,
 				vec2
 			),
-			fcppt::math::pi<float_type>() / 2.f
+			fcppt::math::pi<real>() / 2.f
 		)
 	);
 }
 
 BOOST_AUTO_TEST_CASE(vector_swap)
 {
-	ui2_vec vec1(
+	uivector2 vec1(
 		1u,
 		2u
 	);
 
-	ui2_vec vec2(
+	uivector2 vec2(
 		3u,
 		4u
 	);
@@ -137,7 +151,7 @@ BOOST_AUTO_TEST_CASE(vector_swap)
 	BOOST_REQUIRE(
 		vec1
 		==
-		ui2_vec(
+		uivector2(
 			3u,
 			4u
 		)
@@ -146,7 +160,7 @@ BOOST_AUTO_TEST_CASE(vector_swap)
 	BOOST_REQUIRE(
 		vec2
 		==
-		ui2_vec(
+		uivector2(
 			1u,
 			2u
 		)
@@ -158,11 +172,11 @@ BOOST_AUTO_TEST_CASE(vector_unit_circle)
 	BOOST_REQUIRE(
 		fcppt::math::vector::unit_circle(
 			fcppt::math::pi<
-				float_type
+				real
 			>()
 		)
 		==
-		f2_vec(
+		fvector2(
 			-1.f,
 			0.f
 		)
@@ -172,31 +186,31 @@ BOOST_AUTO_TEST_CASE(vector_unit_circle)
 BOOST_AUTO_TEST_CASE(vector_null)
 {
 	BOOST_REQUIRE(
-		ui2_vec::null()
-		== ui2_vec(0, 0)
+		uivector2::null()
+		== uivector2(0, 0)
 	);
 }
 
 BOOST_AUTO_TEST_CASE(vector_arithmetic)
 {
-	ui2_vec vec(0, 0);
+	uivector2 vec(0, 0);
 
-	vec += ui2_vec(1, 2);
+	vec += uivector2(1, 2);
 
 	BOOST_REQUIRE(
-		vec == ui2_vec(1, 2)
+		vec == uivector2(1, 2)
 	);
 
 	vec *= 2;
 
 	BOOST_REQUIRE(
-		vec == ui2_vec(2, 4)
+		vec == uivector2(2, 4)
 	);
 
 	vec /= 2;
 
 	BOOST_REQUIRE(
-		vec == ui2_vec(1, 2)
+		vec == uivector2(1, 2)
 	);
 }
 
@@ -267,7 +281,7 @@ BOOST_AUTO_TEST_CASE(vector_construct)
 		)
 	);
 
-	ui2_vec const vec(
+	uivector2 const vec(
 		view
 	);
 
@@ -278,20 +292,98 @@ BOOST_AUTO_TEST_CASE(vector_construct)
 
 BOOST_AUTO_TEST_CASE(vector_structure_cast)
 {
-	ui2_vec const vec(
+	uivector2 const vec(
 		1,
 		2
 	);
 
 	BOOST_REQUIRE(
 		fcppt::math::vector::structure_cast<
-			f2_vec
+			fvector2
 		>(
 			vec
 		)
-		== f2_vec(
+		== fvector2(
 			1.f,
 			2.f
 		)
 	);
+}
+
+BOOST_AUTO_TEST_CASE(vector_hypersphere_to_cartesian)
+{
+	real const 
+		phi1 = 
+			static_cast<real>(1.5),
+		phi2 = 
+			static_cast<real>(0.5);
+	
+	fvector2 const result2 = 
+		fcppt::math::vector::hypersphere_to_cartesian(
+			fvector1(
+				phi1));
+
+	fvector3 const result3 = 
+		fcppt::math::vector::hypersphere_to_cartesian(
+			fvector2(
+				phi1,
+				phi2));
+
+	std::cout 
+		<< "Testing the result of 2D hypersphere_to_cartesian.\n"
+		<< "The result should be:\n"
+		<< 
+			fvector2(
+				std::cos(
+					phi1),
+				std::sin(
+					phi1))
+		<< "\n and it is:\n" 
+		<< result2 
+		<< "\n";
+
+	BOOST_CHECK(
+		fcppt::math::range_compare(
+			result2,
+			fvector2(
+				std::cos(
+					phi1),
+				std::sin(
+					phi1)),
+			epsilon));
+
+	std::cout 
+		<< "Testing the result of 3D hypersphere_to_cartesian.\n"
+		<< "The result should be:\n"
+		<< 
+			fvector3(
+				std::cos(
+					phi1),
+				std::sin(
+					phi1) * 
+				std::cos(
+					phi2),
+				std::sin(
+					phi1) *
+				std::sin(
+					phi2))
+		<< "\n and it is:\n" 
+		<< result3
+		<< "\n";
+
+	BOOST_CHECK(
+		fcppt::math::range_compare(
+			result3,
+			fvector3(
+				std::cos(
+					phi1),
+				std::sin(
+					phi1) * 
+				std::cos(
+					phi2),
+				std::sin(
+					phi1) *
+				std::sin(
+					phi2)),
+			epsilon));
 }
