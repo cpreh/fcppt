c28bc1df10f1a7f6c2617b7de5861295e7505a2e
Philipp Reh <sefi@s-e-f-i.de>
2011-03-18 20:50:25 +0100

    Hack fcppt::ref for now.

--- include/fcppt/ref.hpp
+++ include/fcppt/ref.hpp
@@ -7,8 +7,13 @@
 #ifndef FCPPT_REF_HPP_INCLUDED
 #define FCPPT_REF_HPP_INCLUDED
 
+#include <fcppt/detail/use_boost_reference.hpp>
 #include <fcppt/reference_wrapper.hpp>
+#ifdef FCPPT_DETAIL_USE_BOOST_REFERENCE
 #include <boost/ref.hpp>
+#else
+#include <fcppt/tr1/functional.hpp>
+#endif
 
 namespace fcppt
 {
@@ -30,7 +35,11 @@ ref(
 )
 {
 	return
+#ifdef FCPPT_DETAIL_USE_BOOST_REFERENCE
 		boost::ref(
+#else
+		std::tr1::ref(
+#endif
 			_ref
 		);
 }
--- include/fcppt/reference_wrapper.hpp
+++ include/fcppt/reference_wrapper.hpp
@@ -7,12 +7,21 @@
 #ifndef FCPPT_REFERENCE_WRAPPER_HPP_INCLUDED
 #define FCPPT_REFERENCE_WRAPPER_HPP_INCLUDED
 
+#include <fcppt/detail/use_boost_reference.hpp>
+#ifdef FCPPT_DETAIL_USE_BOOST_REFERENCE
 #include <boost/ref.hpp>
+#else
+#include <fcppt/tr1/functional.hpp>
+#endif
 
 namespace fcppt
 {
 
+#ifdef FCPPT_DETAIL_USE_BOOST_REFERENCE
 using boost::reference_wrapper;
+#else
+using std::tr1::reference_wrapper;
+#endif
 
 }
 
