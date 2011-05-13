2a0c34cc133e3f5d36c42243647abaa7038aa9f1
phillemann <pmidden@gmx.net>
2011-05-13 22:48:18 +0200

    Added algorithm::array_map

---
+++ include/fcppt/algorithm/array_map.hpp
@@ -0,0 +1,38 @@
+//          Copyright Carl Philipp Reh 2009 - 2011.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef FCPPT_ALGORITHM_ARRAY_MAP_HPP_INCLUDED
+#define FCPPT_ALGORITHM_ARRAY_MAP_HPP_INCLUDED
+
+#include <algorithm>
+
+namespace fcppt
+{
+namespace algorithm
+{
+template
+<
+	typename TargetArray,
+	typename SourceArray,
+	typename Functor
+>
+TargetArray const
+array_map(
+	SourceArray const &source,
+	Functor const &f)
+{
+	TargetArray result;
+	std::transform(
+		source.begin(),
+		source.end(),
+		result.begin(),
+		f);
+	return result;
+}
+}
+}
+
+#endif
