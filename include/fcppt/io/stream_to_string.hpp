681bc4dfa250db00005b514b29039885f7d3a67f
Philipp Reh <sefi@s-e-f-i.de>
2011-06-08 17:36:01 +0200

    Added io::stream_to_string.

---
+++ include/fcppt/io/stream_to_string.hpp
@@ -0,0 +1,48 @@
+//          Copyright Carl Philipp Reh 2009 - 2011.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef FCPPT_IO_STREAM_TO_STRING_HPP_INCLUDED
+#define FCPPT_IO_STREAM_TO_STRING_HPP_INCLUDED
+
+#include <istream>
+#include <sstream>
+#include <string>
+
+namespace fcppt
+{
+namespace io
+{
+
+/// Extracts a string from @a _stream
+template<
+	typename Ch,
+	typename Traits
+>
+std::basic_string<
+	Ch,
+	Traits
+> const
+stream_to_string(
+	std::basic_istream<
+		Ch,
+		Traits
+	> &_stream
+)
+{
+	std::basic_stringstream<
+		Ch,
+		Traits
+	> string_stream;
+
+	string_stream << _stream.rdbuf();
+
+	return string_stream.str();
+}
+
+}
+}
+
+#endif
---
+++ test/io/stream_to_string.cpp
@@ -0,0 +1,30 @@
+//          Copyright Carl Philipp Reh 2009 - 2011.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <fcppt/io/stream_to_string.hpp>
+#include <fcppt/io/stream_to_string.hpp>
+#include <boost/test/unit_test.hpp>
+#include <string>
+#include <sstream>
+
+BOOST_AUTO_TEST_CASE(
+	io_stream_to_string
+)
+{
+	std::istringstream istream(
+		"TEST abcd"
+	);
+
+	std::string const result(
+		fcppt::io::stream_to_string(
+			istream
+		)
+	);
+
+	BOOST_CHECK(
+		result == "TEST abcd"
+	);
+}
