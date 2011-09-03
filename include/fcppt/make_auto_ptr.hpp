05f20b287cdb7eb4f37d2264cf2c1f2bffb8aca6
Philipp Reh <sefi@s-e-f-i.de>
2011-09-04 01:22:53 +0200

    Removed auto_ptr typedef.

--- include/fcppt/make_auto_ptr.hpp
+++ include/fcppt/make_auto_ptr.hpp
@@ -7,15 +7,15 @@
 #ifndef FCPPT_MAKE_AUTO_PTR_HPP_INCLUDED
 #define FCPPT_MAKE_AUTO_PTR_HPP_INCLUDED
 
-#include <fcppt/auto_ptr.hpp>
 #include <fcppt/detail/make_ptr_base.hpp>
+#include <memory>
 
 namespace fcppt
 {
 
 FCPPT_DETAIL_MAKE_PTR_BASE(
 	auto_ptr,
-	auto_ptr,
+	std::auto_ptr,
 	new Type
 )
 
