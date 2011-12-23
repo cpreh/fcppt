86b40fef44de434ca57c325b890cb7c7bdcea907
Philipp Reh <sefi@s-e-f-i.de>
2011-12-23 14:22:09 +0100

    Documented assert_complete.

---
+++ examples/assert_complete.cpp
@@ -0,0 +1,44 @@
+//          Copyright Carl Philipp Reh 2009 - 2011.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <fcppt/assert_complete.hpp>
+
+//! [assert_complete]
+namespace
+{
+
+template<
+	typename T
+>
+void
+std_delete(
+	T *const _param
+)
+{
+	FCPPT_ASSERT_COMPLETE(
+		T
+	);
+
+	delete _param;
+}
+//! [assert_complete]
+
+struct complete
+{
+};
+
+}
+
+int main()
+{
+	complete *const ptr(
+		new complete
+	);
+
+	std_delete(
+		ptr
+	);
+}
--- include/fcppt/assert_complete.hpp
+++ include/fcppt/assert_complete.hpp
@@ -7,8 +7,28 @@
 #ifndef FCPPT_ASSERT_COMPLETE_HPP_INCLUDED
 #define FCPPT_ASSERT_COMPLETE_HPP_INCLUDED
 
-/// Assert at compile time that a type is complete
-#define FCPPT_ASSERT_COMPLETE(type)\
-static_cast<void>(sizeof(type))
+/**
+\brief Assert at compile time that a type is complete
+
+\ingroup fcpptmain
+
+In some cases, a type must be complete, which bascially means that it is not
+void and if it is a class, the definition must already be known.  For example,
+<code>delete</code> has undefined behaviour if it is invoked on a type that is
+not complete but has a non trivial destructor.
+
+This macro generates a compile time error if a type is not complete. An example
+of its usage is shown below.
+
+\snippet assert_complete.cpp assert_complete
+*/
+#define FCPPT_ASSERT_COMPLETE(\
+	type\
+)\
+static_cast<\
+	void\
+>(\
+	sizeof(type)\
+)
 
 #endif
