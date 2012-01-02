313f598ef12002a3988fc4c77296f5c6048e43af
Philipp Reh <sefi@s-e-f-i.de>
2012-01-02 15:16:12 +0100

    Use std::locale("") for string_conv_locale.

--- src/string_conv_locale.cpp
+++ src/string_conv_locale.cpp
@@ -6,80 +6,12 @@
 
 #include <fcppt/string_conv_locale.hpp>
 #include <fcppt/config/external_begin.hpp>
-#include <cstdlib>
 #include <locale>
 #include <fcppt/config/external_end.hpp>
 
 
-namespace
-{
-
-std::locale global;
-
-char const *
-probe_locale();
-
-struct init
-{
-	init();
-} instance;
-
-}
-
 std::locale const
 fcppt::string_conv_locale()
 {
-	return global;
-}
-
-namespace
-{
-
-char const *
-probe_locale()
-{
-	if(
-		char const *const lc_all =
-			std::getenv(
-				"LC_ALL"
-			)
-	)
-		return lc_all;
-
-	if(
-		char const *const lc_ctype =
-			std::getenv(
-				"LC_CTYPE"
-			)
-	)
-		return lc_ctype;
-
-	/* This doesn't work on (non-english) Windows
-	if(
-		char const *const lang =
-			std::getenv(
-				"LANG"
-			)
-	)
-		return lang;
-		*/
-
-	return 0;
-}
-
-init::init()
-{
-	char const *const locale_name(
-		probe_locale()
-	);
-
-	if(
-		locale_name
-	)
-		global =
-			std::locale(
-				locale_name
-			);
-}
-
+	return std::locale("");
 }
