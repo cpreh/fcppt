339b612434ea4e8955ff1adce9d34755b35b113d
Philipp Reh <sefi@s-e-f-i.de>
2012-03-02 18:28:35 +0100

    Added macros for explicit function/class instantiations.

---
+++ include/fcppt/export_class_instantiation.hpp
@@ -0,0 +1,34 @@
+//          Copyright Carl Philipp Reh 2009 - 2012.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef FCPPT_EXPORT_CLASS_INSTANTIATION_HPP_INCLUDED
+#define FCPPT_EXPORT_CLASS_INSTANTIATION_HPP_INCLUDED
+
+#include <fcppt/config/compiler.hpp>
+
+#if defined(FCPPT_CONFIG_MSVC_COMPILER)
+#	define FCPPT_EXPORT_CLASS_INSTANTIATION_IMPL
+#elif defined(FCPPT_CONFIG_GCC_COMPILER) || defined(FCPPT_CONFIG_ICC_COMPILER)
+#	include <fcppt/export_symbol.hpp>
+#	define FCPPT_EXPORT_CLASS_INSTANTIATION_IMPL FCPPT_EXPORT_SYMBOL
+#else
+#	error "Don't know what FCPPT_EXPORT_CLASS_INSTANTIATION should be"
+#endif
+
+
+/**
+\brief Tells that an explicit class instantiation should be exported
+
+\ingroup fcpptexport
+
+This macro marks an explicitly instantiated template class to be exported, so
+it can be used by other libraries or programs.
+
+\see \ref exports_library
+*/
+#define FCPPT_EXPORT_CLASS_INSTANTIATION FCPPT_EXPORT_CLASS_INSTANTIATION_IMPL
+
+#endif
---
+++ include/fcppt/export_function_instantiation.hpp
@@ -0,0 +1,24 @@
+//          Copyright Carl Philipp Reh 2009 - 2012.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef FCPPT_EXPORT_FUNCTION_INSTANTIATION_HPP_INCLUDED
+#define FCPPT_EXPORT_FUNCTION_INSTANTIATION_HPP_INCLUDED
+
+#include <fcppt/export_symbol.hpp>
+
+/**
+\brief Tells that an explicit function instantiation should be exported
+
+\ingroup fcpptexport
+
+This macro marks an explicitly instantiated template function to be exported,
+so it can be used by other libraries or programs.
+
+\see \ref exports_library
+*/
+#define FCPPT_EXPORT_FUNCTION_INSTANTIATION FCPPT_EXPORT_SYMBOL
+
+#endif
