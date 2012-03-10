011f44801aacdd7ae4a16ce6ef38c19a46d90b32
Philipp Reh <sefi@s-e-f-i.de>
2012-03-10 14:52:49 +0100

    Documented safe_bool

--- examples/safe_bool.cpp
+++ examples/safe_bool.cpp
@@ -14,36 +14,50 @@
 namespace
 {
 
-//[safebool_example
+//! [safe_bool_declaration]
 class my_class
 {
-	FCPPT_SAFE_BOOL(my_class)
+	// Implement the safe bool idiom for this class
+	FCPPT_SAFE_BOOL(
+		my_class
+	)
 private:
+	// We must also implement this function to tell when the object is
+	// supposed to be true.
 	bool
 	boolean_test() const
 	{
 		return isset_;
 	}
 public:
-	explicit my_class(
+	explicit
+	my_class(
 		bool const _isset
 	)
 	:
-		isset_(_isset)
-	{}
+		isset_(
+			_isset
+		)
+	{
+	}
 private:
 	bool isset_;
 };
-//]
+//! [safe_bool_declaration]
 
 }
 
-int main()
+int
+main()
 {
-//[safebool_usage
-	my_class test(true);
+//! [safe_bool_usage]
+	my_class test(
+		true
+	);
 
-	if(test)
+	if(
+		test
+	)
 		std::cout << "test is set\n";
-//]
+//! [safe_bool_usage]
 }
--- include/fcppt/safe_bool.hpp
+++ include/fcppt/safe_bool.hpp
@@ -8,9 +8,40 @@
 #define FCPPT_SAFE_BOOL_HPP_INCLUDED
 
 
+/**
+\brief Implements the safe bool idiom for a class
+
+\ingroup fcpptvarious
+
+Implements the safe bool idiom for a class called \a classname. The macro must
+be placed inside the class definition and called with the name of the class.
+
+It generates a member function that makes it possible to use objects of this
+class as a condition like in <code>if(object)</code>. However, objects
+of this class are not convertible to <code>bool</code> or any other integral
+types.
+
+To tell when the expression <code>object</code> should evaluate to
+<code>true</code>, the member function <code>bool boolean_test() const</code>
+must be implemented.
+
+In the following example, a class is defined using the safe bool idiom:
+
+\snippet safe_bool.cpp safe_bool_declaration
+
+An object of this class can then be used in an if statement:
+
+\snippet safe_bool.cpp safe_bool_usage
+
+\param classname The name of the class
+
+\note The macro leaves a public scope behind.
+*/
 #define FCPPT_SAFE_BOOL(\
 	classname\
 )\
+private: \
+\
 typedef void (classname::*bool_type)() const; \
 \
 void \
