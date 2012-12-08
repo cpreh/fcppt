ce608d08eef137e7b969f24c9211dd6eebf73588
Philipp Reh <sefi@s-e-f-i.de>
2012-12-08 18:14:00 +0100

    Removed assign::make_array

--- examples/doc/assign.cpp
+++ examples/doc/assign.cpp
@@ -4,9 +4,7 @@
 //          http://www.boost.org/LICENSE_1_0.txt)
 
 
-#include <fcppt/assign/make_array.hpp>
 #include <fcppt/assign/make_container.hpp>
-#include <fcppt/container/array.hpp>
 #include <fcppt/config/external_begin.hpp>
 #include <vector>
 #include <fcppt/config/external_end.hpp>
@@ -14,15 +12,6 @@
 int
 main()
 {
-{
-//! [make_array]
-fcppt::container::array<int,3> result;
-result = fcppt::assign::make_array<int>(1)(2)(3);
-// Result now contains 1,2,3
-result = fcppt::assign::make_array<int>(1)(2)(3).container();
-// The same, but more explicit (might be needed inside templates)
-//! [make_array]
-}
 
 {
 //! [make_container]
