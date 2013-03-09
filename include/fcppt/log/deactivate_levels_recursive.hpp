36a24ce175b8e704becbae85afb3ca9c5b532217
Philipp Reh <sefi@s-e-f-i.de>
2013-03-09 16:14:03 +0100

    Added log::deactivate_levels_recursive

---
+++ include/fcppt/log/deactivate_levels_recursive.hpp
@@ -0,0 +1,44 @@
+//          Copyright Carl Philipp Reh 2009 - 2013.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef FCPPT_LOG_DEACTIVATE_LEVELS_RECURSIVE_HPP_INCLUDED
+#define FCPPT_LOG_DEACTIVATE_LEVELS_RECURSIVE_HPP_INCLUDED
+
+#include <fcppt/symbol.hpp>
+#include <fcppt/log/context_fwd.hpp>
+#include <fcppt/log/level_fwd.hpp>
+#include <fcppt/log/location_fwd.hpp>
+
+
+namespace fcppt
+{
+namespace log
+{
+
+/**
+\brief Deactivates all the log levels that are equal or higher in all loggers.
+
+\ingroup fcpptlog
+
+Deactivates all log levels starting at and including \a level for all loggers
+that are reachable from \a location,
+
+\param context The logger context
+\param location The location of the start logger
+\param level The lower bound of the levels to deactivate
+*/
+FCPPT_SYMBOL
+void
+deactivate_levels_recursive(
+	fcppt::log::context &context,
+	fcppt::log::location const &location,
+	fcppt::log::level level
+);
+
+}
+}
+
+#endif
