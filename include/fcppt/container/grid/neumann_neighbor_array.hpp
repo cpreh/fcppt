a57e678d7e5e1c44a560adcdaf44fd5b67f7ef76
Philipp Reh <sefi@s-e-f-i.de>
2013-09-28 18:01:46 +0200

    Added neumann neighbor function to grid

---
+++ include/fcppt/container/grid/neumann_neighbor_array.hpp
@@ -0,0 +1,46 @@
+//          Copyright Carl Philipp Reh 2009 - 2013.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef FCPPT_CONTAINER_GRID_NEUMANN_NEIGHBOR_ARRAY_HPP_INCLUDED
+#define FCPPT_CONTAINER_GRID_NEUMANN_NEIGHBOR_ARRAY_HPP_INCLUDED
+
+#include <fcppt/container/grid/pos.hpp>
+#include <fcppt/container/grid/size_type.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <array>
+#include <fcppt/config/external_end.hpp>
+
+
+namespace fcppt
+{
+namespace container
+{
+namespace grid
+{
+
+/**
+\ingroup fcpptcontainergrid
+
+\brief The type return by fcppt::container::grid::neumann_neighbors
+
+\tparam Pos Must be an fcppt::container::grid::pos
+*/
+template<
+	typename Pos
+>
+using
+neumann_neighbor_array
+=
+std::array<
+	Pos,
+	4
+>;
+
+}
+}
+}
+
+#endif
---
+++ include/fcppt/container/grid/neumann_neighbors.hpp
@@ -0,0 +1,84 @@
+//          Copyright Carl Philipp Reh 2009 - 2013.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef FCPPT_CONTAINER_GRID_NEUMANN_NEIGHBORS_HPP_INCLUDED
+#define FCPPT_CONTAINER_GRID_NEUMANN_NEIGHBORS_HPP_INCLUDED
+
+#include <fcppt/container/grid/neumann_neighbor_array.hpp>
+#include <fcppt/container/grid/pos.hpp>
+#include <fcppt/container/grid/size_type.hpp>
+
+
+namespace fcppt
+{
+namespace container
+{
+namespace grid
+{
+
+/**
+\ingroup fcpptcontainergrid
+
+\brief Computes the von Neumann neighbors
+
+Computes the von Neumann neighbors of \a _pos. No range checking is performed.
+
+\param _pos The position to compute the neighbors of
+
+\return An array with four elements, containing the neighbors
+*/
+template<
+	typename T,
+	fcppt::container::grid::size_type N
+>
+fcppt::container::grid::neumann_neighbor_array<
+	fcppt::container::grid::pos<
+		T,
+		N
+	>
+>
+neumann_neighbors(
+	fcppt::container::grid::pos<
+		T,
+		N
+	> const _pos
+)
+{
+	typedef
+	fcppt::container::grid::pos<
+		T,
+		N
+	>
+	pos_type;
+
+	return
+		fcppt::container::grid::neumann_neighbor_array<
+			pos_type
+		>{{
+			pos_type{
+				_pos.x() - 1,
+				_pos.y()
+			},
+			pos_type{
+				_pos.x() + 1,
+				_pos.y()
+			},
+			pos_type{
+				_pos.x(),
+				_pos.y() - 1,
+			},
+			pos_type{
+				_pos.x(),
+				_pos.y() + 1
+			}
+		}};
+}
+
+}
+}
+}
+
+#endif
