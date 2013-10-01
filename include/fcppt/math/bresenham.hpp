d03350acf024ebf6b067f7821fbaff80d2e7d1fe
Philipp Reh <sefi@s-e-f-i.de>
2013-10-01 14:28:15 +0200

    Added bresenham from sanguis

---
+++ include/fcppt/math/bresenham.hpp
@@ -0,0 +1,237 @@
+//          Copyright Carl Philipp Reh 2009 - 2013.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef FCPPT_MATH_BRESENHAM_HPP_INCLUDED
+#define FCPPT_MATH_BRESENHAM_HPP_INCLUDED
+
+#include <fcppt/literal.hpp>
+#include <fcppt/math/diff.hpp>
+#include <fcppt/math/vector/object_impl.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <algorithm>
+#include <type_traits>
+#include <fcppt/config/external_end.hpp>
+
+
+namespace fcppt
+{
+namespace math
+{
+
+template<
+	typename T,
+	typename N,
+	typename S1,
+	typename S2,
+	typename Callback
+>
+typename
+std::enable_if<
+	N::value
+	==
+	2,
+	void
+>::type
+bresenham(
+	fcppt::math::vector::object<
+		T,
+		N,
+		S1
+	> const _start,
+	fcppt::math::vector::object<
+		T,
+		N,
+		S2
+	> const _end,
+	Callback const &_callback
+)
+{
+	static_assert(
+		std::is_signed<
+			T
+		>::value,
+		"bresenham currently only works on signed types"
+	);
+
+	T x0(
+		_start.x()
+	);
+
+	T y0(
+		_start.y()
+	);
+
+	T x1(
+		_end.x()
+	);
+
+	T y1(
+		_end.y()
+	);
+
+	bool const steep =
+		fcppt::math::diff(
+			y1,
+			y0
+		)
+		>
+		fcppt::math::diff(
+			x1,
+			x0
+		);
+
+	if(
+		steep
+	)
+	{
+		std::swap(
+			x0,
+			y0
+		);
+
+		std::swap(
+			x1,
+			y1
+		);
+	}
+
+	if(
+		x0 > x1
+	)
+	{
+		std::swap(
+			x0,
+			x1
+		);
+
+		std::swap(
+			y0,
+			y1
+		);
+	}
+
+	T const dx(
+		x1 - x0
+	);
+
+	T const dy(
+		fcppt::math::diff(
+			y1,
+			y0
+		)
+	);
+
+	T diff(
+		fcppt::literal<
+			T
+		>(
+			2
+		)
+		* dy
+		- dx
+	);
+
+	typedef
+	fcppt::math::vector::object<
+		T,
+		N,
+		fcppt::math::static_storage<
+			T,
+			N
+		>
+	>
+	vector_type;
+
+	_callback(
+		vector_type(
+			x0,
+			y0
+		)
+	);
+
+	T y(
+		y0
+	);
+
+	T const two(
+		fcppt::literal<
+			T
+		>(
+			2
+		)
+	);
+
+	for(
+		T x(
+			x0
+			+
+			fcppt::literal<
+				T
+			>(
+				1
+			)
+		);
+		x <= x1;
+		++x
+	)
+	{
+		if(
+			diff
+			>
+			fcppt::literal<
+				T
+			>(
+				0
+			)
+		)
+		{
+			++y;
+
+			_callback(
+				steep
+				?
+					vector_type(
+						y,
+						x
+					)
+				:
+					vector_type(
+						x,
+						y
+					)
+				);
+
+			diff +=
+				two * dy
+				-
+				two * dx;
+		}
+		else
+		{
+			_callback(
+				steep
+				?
+					vector_type(
+						y,
+						x
+					)
+				:
+					vector_type(
+						x,
+						y
+					)
+			);
+
+			diff +=
+				two * dy;
+		}
+	}
+}
+
+}
+}
+
+#endif
---
+++ test/math/bresenham.cpp
@@ -0,0 +1,169 @@
+//          Copyright Carl Philipp Reh 2009 - 2013.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <fcppt/math/bresenham.hpp>
+#include <fcppt/math/vector/comparison.hpp>
+#include <fcppt/math/vector/object_impl.hpp>
+#include <fcppt/math/vector/static.hpp>
+#include <fcppt/preprocessor/disable_gcc_warning.hpp>
+#include <fcppt/preprocessor/pop_warning.hpp>
+#include <fcppt/preprocessor/push_warning.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <boost/test/unit_test.hpp>
+#include <vector>
+#include <fcppt/config/external_end.hpp>
+
+
+FCPPT_PP_PUSH_WARNING
+FCPPT_PP_DISABLE_GCC_WARNING(-Weffc++)
+
+BOOST_AUTO_TEST_CASE(
+	math_bresenham
+)
+{
+FCPPT_PP_POP_WARNING
+
+	typedef
+	fcppt::math::vector::static_<
+		int,
+		2
+	>
+	int2_vector;
+
+	typedef
+	std::vector<
+		int2_vector
+	>
+	int2_vector_sequence;
+
+	int2_vector_sequence result;
+
+	fcppt::math::bresenham(
+		int2_vector(
+			0,
+			0
+		),
+		int2_vector(
+			10,
+			4
+		),
+		[
+			&result
+		](
+			int2_vector const _vec
+		)
+		{
+			result.push_back(
+				_vec
+			);
+		}
+	);
+
+	BOOST_REQUIRE(
+		result.size()
+		==
+		11u
+	);
+
+	BOOST_CHECK(
+		result[0]
+		==
+		int2_vector(
+			0,
+			0
+		)
+	);
+
+	BOOST_CHECK(
+		result[1]
+		==
+		int2_vector(
+			1,
+			0
+		)
+	);
+
+	BOOST_CHECK(
+		result[2]
+		==
+		int2_vector(
+			2,
+			1
+		)
+	);
+
+	BOOST_CHECK(
+		result[3]
+		==
+		int2_vector(
+			3,
+			1
+		)
+	);
+
+	BOOST_CHECK(
+		result[4]
+		==
+		int2_vector(
+			4,
+			2
+		)
+	);
+
+	BOOST_CHECK(
+		result[5]
+		==
+		int2_vector(
+			5,
+			2
+		)
+	);
+
+	BOOST_CHECK(
+		result[6]
+		==
+		int2_vector(
+			6,
+			2
+		)
+	);
+
+	BOOST_CHECK(
+		result[7]
+		==
+		int2_vector(
+			7,
+			3
+		)
+	);
+
+	BOOST_CHECK(
+		result[8]
+		==
+		int2_vector(
+			8,
+			3
+		)
+	);
+
+	BOOST_CHECK(
+		result[9]
+		==
+		int2_vector(
+			9,
+			4
+		)
+	);
+
+	BOOST_CHECK(
+		result[10]
+		==
+		int2_vector(
+			10,
+			4
+		)
+	);
+}
