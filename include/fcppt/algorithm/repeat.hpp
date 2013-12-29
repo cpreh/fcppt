23e37afbc2d754d94dc1a341ad9e425dbb7c25c5
Philipp Reh <sefi@s-e-f-i.de>
2013-12-29 20:34:45 +0100

    Added algorithm::repeat

---
+++ include/fcppt/algorithm/repeat.hpp
@@ -0,0 +1,45 @@
+//          Copyright Carl Philipp Reh 2009 - 2013.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef FCPPT_ALGORITHM_REPEAT_HPP_INCLUDED
+#define FCPPT_ALGORITHM_REPEAT_HPP_INCLUDED
+
+#include <fcppt/literal.hpp>
+
+
+namespace fcppt
+{
+namespace algorithm
+{
+
+template<
+	typename Count,
+	typename Function
+>
+void
+repeat(
+	Count const _count,
+	Function const &_function
+)
+{
+	for(
+		Count index(
+			fcppt::literal<
+				Count
+			>(
+				0
+			)
+		);
+		index < _count;
+		++index
+	)
+		_function();
+}
+
+}
+}
+
+#endif
---
+++ test/algorithm/repeat.cpp
@@ -0,0 +1,44 @@
+//          Copyright Carl Philipp Reh 2009 - 2013.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <fcppt/algorithm/repeat.hpp>
+#include <fcppt/preprocessor/disable_gcc_warning.hpp>
+#include <fcppt/preprocessor/pop_warning.hpp>
+#include <fcppt/preprocessor/push_warning.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <boost/test/unit_test.hpp>
+#include <fcppt/config/external_end.hpp>
+
+
+FCPPT_PP_PUSH_WARNING
+FCPPT_PP_DISABLE_GCC_WARNING(-Weffc++)
+
+BOOST_AUTO_TEST_CASE(
+	algorithm_repeat
+)
+{
+FCPPT_PP_POP_WARNING
+
+	int value(
+		0
+	);
+
+	fcppt::algorithm::repeat(
+		10,
+		[
+			&value
+		]()
+		{
+			++value;
+		}
+	);
+
+	BOOST_CHECK(
+		value
+		==
+		10
+	);
+}
