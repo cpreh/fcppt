e72ba68ede7f2441af9ea2b6159d2340c6f4420a
Philipp Reh <sefi@s-e-f-i.de>
2014-04-04 14:43:43 +0200

    Added filesystem::normalize function

---
+++ include/fcppt/filesystem/normalize.hpp
@@ -0,0 +1,39 @@
+//          Copyright Carl Philipp Reh 2009 - 2013.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef FCPPT_FILESYSTEM_NORMALIZE_HPP_INCLUDED
+#define FCPPT_FILESYSTEM_NORMALIZE_HPP_INCLUDED
+
+#include <fcppt/filesystem/symbol.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <boost/filesystem/path.hpp>
+#include <fcppt/config/external_end.hpp>
+
+
+namespace fcppt
+{
+namespace filesystem
+{
+
+/**
+\brief Normalizes a path by removing trailing slashes
+
+\ingroup fcpptfilesystem
+
+Returns a copy of \a path with trailing slashes removed.
+
+\param path The path to remove trailing slashes from
+*/
+FCPPT_FILESYSTEM_SYMBOL
+boost::filesystem::path
+normalize(
+	boost::filesystem::path const &path
+);
+
+}
+}
+
+#endif
---
+++ src/filesystem/normalize.cpp
@@ -0,0 +1,33 @@
+//          Copyright Carl Philipp Reh 2009 - 2013.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <fcppt/text.hpp>
+#include <fcppt/filesystem/normalize.hpp>
+#include <fcppt/filesystem/stem.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <boost/filesystem/path.hpp>
+#include <fcppt/config/external_end.hpp>
+
+
+boost::filesystem::path
+fcppt::filesystem::normalize(
+	boost::filesystem::path const &_path
+)
+{
+	return
+		fcppt::filesystem::stem(
+			_path
+		)
+		==
+		FCPPT_TEXT(".")
+		?
+			boost::filesystem::path(
+				_path
+			).remove_filename()
+		:
+			_path
+		;
+}
