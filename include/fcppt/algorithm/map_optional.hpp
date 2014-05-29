d16f4b94d49ead15cb88e46775bc6332fc6b0e88
Philipp Reh <sefi@s-e-f-i.de>
2014-05-29 13:25:25 +0200

    Added map_optional

---
+++ include/fcppt/algorithm/map_optional.hpp
@@ -0,0 +1,85 @@
+//          Copyright Carl Philipp Reh 2009 - 2013.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef FCPPT_ALGORITHM_MAP_OPTIONAL_HPP_INCLUDED
+#define FCPPT_ALGORITHM_MAP_OPTIONAL_HPP_INCLUDED
+
+#include <fcppt/is_optional.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <type_traits>
+#include <utility>
+#include <fcppt/config/external_end.hpp>
+
+
+namespace fcppt
+{
+namespace algorithm
+{
+
+/**
+\ingroup fcpptalgorithm
+*/
+template
+<
+	typename TargetContainer,
+	typename SourceContainer,
+	typename Functor
+>
+TargetContainer
+map_optional(
+	SourceContainer const &_source,
+	Functor const &_function
+)
+{
+	typedef
+	typename
+	std::result_of<
+		Functor(
+			typename SourceContainer::value_type const &
+		)
+	>::type
+	result_type;
+
+	static_assert(
+		fcppt::is_optional<
+			result_type
+		>::value,
+		"map_optional requires a function that returns an optional"
+	);
+
+	TargetContainer result;
+
+	for(
+		auto const &element
+		:
+		_source
+	)
+	{
+		auto result_element(
+			_function(
+				element
+			)
+		);
+
+		if(
+			result_element
+		)
+			result.insert(
+				result.end(),
+				std::move(
+					*result_element
+				)
+			);
+	}
+
+	return
+		result;
+}
+
+}
+}
+
+#endif
---
+++ test/algorithm/map_optional.cpp
@@ -0,0 +1,95 @@
+//          Copyright Carl Philipp Reh 2009 - 2013.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <fcppt/make_unique_ptr.hpp>
+#include <fcppt/optional_impl.hpp>
+#include <fcppt/algorithm/map_optional.hpp>
+#include <fcppt/preprocessor/disable_gcc_warning.hpp>
+#include <fcppt/preprocessor/pop_warning.hpp>
+#include <fcppt/preprocessor/push_warning.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <boost/test/unit_test.hpp>
+#include <memory>
+#include <vector>
+#include <fcppt/config/external_end.hpp>
+
+
+FCPPT_PP_PUSH_WARNING
+FCPPT_PP_DISABLE_GCC_WARNING(-Weffc++)
+
+BOOST_AUTO_TEST_CASE(
+	algorithm_map_optional
+)
+{
+FCPPT_PP_POP_WARNING
+	typedef
+	std::vector<
+		int
+	>
+	int_vector;
+
+	typedef
+	std::unique_ptr<
+		int
+	>
+	int_unique_ptr;
+
+	typedef
+	std::vector<
+		int_unique_ptr
+	>
+	int_unique_ptr_vector;
+
+	int_unique_ptr_vector const result(
+		fcppt::algorithm::map_optional<
+			int_unique_ptr_vector
+		>(
+			int_vector{
+				1,
+				2,
+				3
+			},
+			[](
+				int const _value
+			)
+			{
+				typedef
+				fcppt::optional<
+					int_unique_ptr
+				>
+				optional_ptr;
+
+				return
+					_value
+					==
+					1
+					?
+						optional_ptr(
+							fcppt::make_unique_ptr<
+								int
+							>(
+								_value
+							)
+						)
+					:
+						optional_ptr()
+					;
+			}
+		)
+	);
+
+	BOOST_REQUIRE(
+		result.size()
+		==
+		1u
+	);
+
+	BOOST_CHECK(
+		*result[0]
+		==
+		1
+	);
+}
