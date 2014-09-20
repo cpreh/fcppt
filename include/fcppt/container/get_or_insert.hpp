1faf2af01c8e24b01a56b28ad39c833af3829569
Carl Philipp Reh <carlphilippreh@gmail.com>
2014-09-20 15:50:47 +0200

    Added container::get_or_insert

---
+++ include/fcppt/container/get_or_insert.hpp
@@ -0,0 +1,82 @@
+//          Copyright Carl Philipp Reh 2009 - 2014.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef FCPPT_CONTAINER_GET_OR_INSERT_HPP_INCLUDED
+#define FCPPT_CONTAINER_GET_OR_INSERT_HPP_INCLUDED
+
+#include <fcppt/from_optional.hpp>
+#include <fcppt/container/find_opt.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <utility>
+#include <fcppt/config/external_end.hpp>
+
+
+namespace fcppt
+{
+namespace container
+{
+
+/**
+\brief Gets are inserts an element from a map
+
+\ingroup fcpptcontainer
+
+Tries to find \a _key in \a _container. If the mapped object is found, it is
+returned.  Otherwise, \a _create is called with _a key to create a new mapped
+object which is then inserted into \a _container and returned.
+
+\tparam Container An associative container
+
+\tparam Create A function of type <code>Container::mapped_type &
+(Container::key_type)</code>
+*/
+template<
+	typename Container,
+	typename Create
+>
+typename
+Container::mapped_type &
+get_or_insert(
+	Container &_container,
+	typename Container::key_type const &_key,
+	Create const &_create
+)
+{
+	return
+		fcppt::from_optional(
+			fcppt::container::find_opt(
+				_container,
+				_key
+			),
+			[
+				&_container,
+				&_create,
+				&_key
+			]()
+			->
+			typename Container::mapped_type &
+			{
+				auto const inserted(
+					_container.insert(
+						std::make_pair(
+							_key,
+							_create(
+								_key
+							)
+						)
+					)
+				);
+
+				return
+					inserted.first->second;
+			}
+		);
+}
+
+}
+}
+
+#endif
---
+++ test/container/get_or_insert.cpp
@@ -0,0 +1,92 @@
+//          Copyright Carl Philipp Reh 2009 - 2014.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <fcppt/string.hpp>
+#include <fcppt/container/get_or_insert.hpp>
+#include <fcppt/preprocessor/disable_gcc_warning.hpp>
+#include <fcppt/preprocessor/pop_warning.hpp>
+#include <fcppt/preprocessor/push_warning.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <boost/test/unit_test.hpp>
+#include <map>
+#include <string>
+#include <fcppt/config/external_end.hpp>
+
+
+FCPPT_PP_PUSH_WARNING
+FCPPT_PP_DISABLE_GCC_WARNING(-Weffc++)
+
+BOOST_AUTO_TEST_CASE(
+	container_get_or_insert
+)
+{
+FCPPT_PP_POP_WARNING
+
+	typedef
+	std::map<
+		int,
+		std::string
+	>
+	int_string_map;
+
+	int_string_map map;
+
+	auto const inserter(
+		[](
+			int const _value
+		)
+		{
+			return
+				std::to_string(
+					_value
+				);
+		}
+	);
+
+	std::string &inserted(
+		fcppt::container::get_or_insert(
+			map,
+			42,
+			inserter
+		)
+	);
+
+	BOOST_REQUIRE(
+		map.find(
+			42
+		)
+		!=
+		map.end()
+	);
+
+	BOOST_CHECK(
+		&inserted
+		==
+		&map.find(
+			42
+		)->second
+	);
+
+	BOOST_CHECK(
+		map.find(
+			42
+		)->second
+		==
+		"42"
+	);
+
+	BOOST_CHECK(
+		fcppt::container::get_or_insert(
+			map,
+			42,
+			inserter
+		)
+		==
+		map.find(
+			42
+		)->second
+	);
+}
