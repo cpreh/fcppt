db70bd09e3abeeca444b236e886349cc142c4682
Carl Philipp Reh <carlphilippreh@gmail.com>
2015-04-04 13:59:11 +0200

    Added const_ function

---
+++ include/fcppt/const.hpp
@@ -0,0 +1,55 @@
+//          Copyright Carl Philipp Reh 2009 - 2015.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef FCPPT_CONST_HPP_INCLUDED
+#define FCPPT_CONST_HPP_INCLUDED
+
+#include <fcppt/detail/const.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <type_traits>
+#include <utility>
+#include <fcppt/config/external_end.hpp>
+
+
+namespace fcppt
+{
+
+/**
+\brief Function returning a constant
+
+\ingroup fcpptvarious
+*/
+template<
+	typename Type
+>
+inline
+fcppt::detail::const_<
+	Type
+>
+const_(
+	Type _value
+)
+{
+	static_assert(
+		!std::is_lvalue_reference<
+			Type
+		>::value,
+		"Type must be a non-reference type"
+	);
+
+	return
+		fcppt::detail::const_<
+			Type
+		>(
+			std::move(
+				_value
+			)
+		);
+}
+
+}
+
+#endif
---
+++ include/fcppt/detail/const.hpp
@@ -0,0 +1,46 @@
+//          Copyright Carl Philipp Reh 2009 - 2015.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef FCPPT_DETAIL_CONST_HPP_INCLUDED
+#define FCPPT_DETAIL_CONST_HPP_INCLUDED
+
+
+namespace fcppt
+{
+namespace detail
+{
+
+template<
+	typename Type
+>
+class const_
+{
+public:
+	explicit
+	const_(
+		Type _value
+	)
+	:
+		value_(
+			_value
+		)
+	{
+	}
+
+	Type
+	operator()() const
+	{
+		return
+			value_;
+	}
+private:
+	Type value_;
+};
+
+}
+}
+
+#endif
