bd068a5bdbd2217733fb6c311cd3c3425459e844
Carl Philipp Reh <carlphilippreh@gmail.com>
2015-04-05 20:03:23 +0200

    Added the identity function

---
+++ include/fcppt/identity.hpp
@@ -0,0 +1,51 @@
+//          Copyright Carl Philipp Reh 2009 - 2015.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef FCPPT_IDENTITY_HPP_INCLUDED
+#define FCPPT_IDENTITY_HPP_INCLUDED
+
+#include <fcppt/config/external_begin.hpp>
+#include <utility>
+#include <fcppt/config/external_end.hpp>
+
+
+namespace fcppt
+{
+
+/**
+\brief The identity function
+
+\ingroup fcpptvarious
+*/
+struct identity
+{
+	template<
+		typename T
+	>
+	auto
+	operator()(
+		T &&_t
+	) const
+	-> decltype(
+		std::forward<
+			T
+		>(
+			_t
+		)
+	)
+	{
+		return
+			std::forward<
+				T
+			>(
+				_t
+			);
+	}
+};
+
+}
+
+#endif
