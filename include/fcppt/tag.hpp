ae223e02f2b00491f3e83b9dabf280e13442bd9e
Carl Philipp Reh <carlphilippreh@gmail.com>
2015-07-26 11:44:39 -0700

    Added fcppt::tag to pass a type as a value

---
+++ include/fcppt/tag.hpp
@@ -0,0 +1,32 @@
+//          Copyright Carl Philipp Reh 2009 - 2015.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef FCPPT_TAG_HPP_INCLUDED
+#define FCPPT_TAG_HPP_INCLUDED
+
+#include <fcppt/tag_fwd.hpp>
+
+
+namespace fcppt
+{
+
+/**
+\brief A value for a type name
+
+\ingroup fcpptvarious
+
+This class can be used to pass the type \a Type as a value.
+*/
+template<
+	typename Type
+>
+struct tag
+{
+};
+
+}
+
+#endif
