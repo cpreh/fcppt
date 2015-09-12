a72e877583f2412eff4021e1fa88ba1b3c573cf2
Carl Philipp Reh <carlphilippreh@gmail.com>
2015-09-12 15:07:45 +0200

    Add tests for set_difference and set_union

---
+++ test/algorithm/set_union.cpp
@@ -0,0 +1,47 @@
+//          Copyright Carl Philipp Reh 2009 - 2015.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <fcppt/algorithm/set_union.hpp>
+#include <fcppt/preprocessor/disable_gcc_warning.hpp>
+#include <fcppt/preprocessor/pop_warning.hpp>
+#include <fcppt/preprocessor/push_warning.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <boost/test/unit_test.hpp>
+#include <set>
+#include <fcppt/config/external_end.hpp>
+
+
+
+FCPPT_PP_PUSH_WARNING
+FCPPT_PP_DISABLE_GCC_WARNING(-Weffc++)
+
+BOOST_AUTO_TEST_CASE(
+	algorithm_set_union
+)
+{
+FCPPT_PP_POP_WARNING
+
+	typedef
+	std::set<
+		int
+	>
+	int_set;
+
+	BOOST_CHECK((
+		fcppt::algorithm::set_union(
+			int_set{
+				1, 2, 3
+			},
+			int_set{
+				2, 3, 4
+			}
+		)
+		==
+		int_set{
+			1, 2, 3, 4
+		}
+	));
+}
