cac60123168d387c9efa6ef4d6346f60c1ea4548
Carl Philipp Reh <carlphilippreh@gmail.com>
2015-11-29 10:21:42 +0100

    Move code from coding_style to examples/

---
+++ examples/coding_style.cpp
@@ -0,0 +1,92 @@
+//          Copyright Carl Philipp Reh 2009 - 2015.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <fcppt/make_int_range.hpp>
+#include <fcppt/algorithm/array_fold_static.hpp>
+#include <fcppt/algorithm/fold.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <array>
+#include <cstddef>
+#include <fcppt/config/external_end.hpp>
+
+
+namespace
+{
+
+void
+coding_style_loop(
+	int const n
+)
+{
+// ![coding_style_loop]
+int result{0};
+for(int i{1}; i < n; ++i) result += i;
+// ![coding_style_loop]
+}
+
+void
+coding_style_fold(
+	int const n
+)
+{
+// ![coding_style_fold]
+fcppt::algorithm::fold(
+	fcppt::make_int_range(1,n),
+	0,
+	[](int i, int result) { return i + result; }
+)
+// ![coding_style_fold]
+;
+}
+
+template<
+	typename T
+>
+T
+from_int(
+	std::size_t const _value
+)
+{
+	return
+		static_cast<
+			T
+		>(
+			_value
+		);
+}
+
+// ![coding_style_make_array]
+template<typename T, std::size_t N>
+std::array<T,N> make_array();
+// ![coding_style_make_array]
+
+template<typename T, std::size_t N>
+std::array<T,N> make_array()
+{
+// ![coding_style_array_fold]
+return fcppt::algorithm::array_fold_static<std::array<T,N>>(
+	[](std::size_t i) { return from_int<T>(i); });
+// ![coding_style_array_fold]
+}
+
+}
+
+int
+main()
+{
+	coding_style_loop(
+		42
+	);
+
+	coding_style_fold(
+		42
+	);
+
+	make_array<
+		int,
+		4
+	>();
+}
---
+++ examples/coding_style_asthetic.cpp
@@ -0,0 +1,100 @@
+//          Copyright Carl Philipp Reh 2009 - 2015.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+namespace
+{
+
+typedef
+int
+result_type;
+
+typedef
+int
+T1;
+
+typedef
+int
+T2;
+
+int
+expression1(
+	int,
+	int
+)
+{
+	return
+		0;
+}
+
+int
+expression2(
+	int
+)
+{
+	return
+		0;
+}
+
+// ![function_example]
+result_type
+my_function(
+	T1 _arg1,
+	T2 _arg2
+)
+{
+	return
+		expression1(
+			expression2(
+				_arg1
+			),
+			_arg2
+		);
+}
+// ![function_example]
+
+// ![struct_example]
+struct test
+{
+	explicit
+	test(
+		int const _arg
+	)
+	:
+		value_(
+			_arg
+		)
+	{
+	}
+
+	int value_;
+};
+
+test
+make_test(
+	int const _arg
+)
+{
+	return
+		test(
+			_arg
+		);
+}
+// ![struct_example]
+
+}
+
+int
+main()
+{
+	my_function(
+		1,
+		2
+	);
+
+	make_test(
+		42
+	);
+}
