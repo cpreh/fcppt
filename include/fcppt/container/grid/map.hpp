cccb162054f5805b98ffeb870f7c3d49f4115cca
Carl Philipp Reh <carlphilippreh@gmail.com>
2015-12-27 13:58:46 +0100

    Added grid::map

---
+++ include/fcppt/container/grid/map.hpp
@@ -0,0 +1,97 @@
+//          Copyright Carl Philipp Reh 2009 - 2015.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef FCPPT_CONTAINER_GRID_MAP_HPP_INCLUDED
+#define FCPPT_CONTAINER_GRID_MAP_HPP_INCLUDED
+
+#include <fcppt/move_if_rvalue.hpp>
+#include <fcppt/container/grid/is_object.hpp>
+#include <fcppt/container/grid/object_impl.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <type_traits>
+#include <fcppt/config/external_end.hpp>
+
+
+namespace fcppt
+{
+namespace container
+{
+namespace grid
+{
+
+/**
+\brief Maps over grids
+
+\ingroup fcpptcontainergrid
+
+For every element <code>x</code> at position <code>p</code> in \a _source, the
+result of <code>_function(x)</code> is inserted at <code>p</code> into the
+result.
+
+\tparam Function A function callable as <code>Result::value_type (Source::value_type)</code>
+*/
+template<
+	typename Result,
+	typename Source,
+	typename Function
+>
+Result
+map(
+	Source &&_source,
+	Function const &_function
+)
+{
+	static_assert(
+		fcppt::container::grid::is_object<
+			Result
+		>::value,
+		"Result must be a grid"
+	);
+
+	typedef
+	typename
+	std::decay<
+		Source
+	>::type
+	source_type;
+
+	static_assert(
+		fcppt::container::grid::is_object<
+			source_type
+		>::value,
+		"Source must be a grid"
+	);
+
+	return
+		Result(
+			_source.size(),
+			[
+				&_source,
+				&_function
+			](
+				typename
+				source_type::pos const _pos
+			)
+			{
+				return
+					_function(
+						fcppt::move_if_rvalue<
+							Source
+						>(
+							_source[
+								_pos
+							]
+						)
+					);
+			}
+		);
+}
+
+}
+}
+}
+
+#endif
---
+++ test/container/grid/map.cpp
@@ -0,0 +1,235 @@
+//          Copyright Carl Philipp Reh 2009 - 2015.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <fcppt/make_unique_ptr.hpp>
+#include <fcppt/strong_typedef.hpp>
+#include <fcppt/unique_ptr.hpp>
+#include <fcppt/cast/size.hpp>
+#include <fcppt/container/grid/map.hpp>
+#include <fcppt/container/grid/object.hpp>
+#include <fcppt/math/at_c.hpp>
+#include <fcppt/math/dim/comparison.hpp>
+#include <fcppt/math/dim/output.hpp>
+#include <fcppt/preprocessor/disable_gcc_warning.hpp>
+#include <fcppt/preprocessor/pop_warning.hpp>
+#include <fcppt/preprocessor/push_warning.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <boost/test/unit_test.hpp>
+#include <string>
+#include <utility>
+#include <fcppt/config/external_end.hpp>
+
+
+FCPPT_PP_PUSH_WARNING
+FCPPT_PP_DISABLE_GCC_WARNING(-Weffc++)
+
+BOOST_AUTO_TEST_CASE(
+	container_grid_map
+)
+{
+FCPPT_PP_POP_WARNING
+
+	typedef
+	fcppt::container::grid::object<
+		std::string,
+		2
+	>
+	string_grid;
+
+	typedef
+	fcppt::container::grid::object<
+		unsigned,
+		2
+	>
+	uint_grid;
+
+	uint_grid const grid(
+		uint_grid::dim{
+			2u,
+			3u
+		},
+		[](
+			uint_grid::pos const _pos
+		)
+		{
+			return
+				fcppt::cast::size<
+					unsigned
+				>(
+					fcppt::math::at_c<
+						0
+					>(
+						_pos
+					)
+					+
+					fcppt::math::at_c<
+						1
+					>(
+						_pos
+					)
+				);
+		}
+	);
+
+	string_grid const result(
+		fcppt::container::grid::map<
+			string_grid
+		>(
+			grid,
+			[](
+				unsigned const _value
+			)
+			{
+				return
+					std::to_string(
+						_value
+					);
+			}
+		)
+	);
+
+	BOOST_REQUIRE_EQUAL(
+		grid.size(),
+		result.size()
+	);
+
+	BOOST_CHECK_EQUAL(
+		result[
+			string_grid::pos(
+				0u,
+				0u
+			)
+		],
+		std::string(
+			"0"
+		)
+	);
+
+	BOOST_CHECK_EQUAL(
+		result[
+			string_grid::pos(
+				1u,
+				2u
+			)
+		],
+		std::string(
+			"3"
+		)
+	);
+}
+
+FCPPT_PP_PUSH_WARNING
+FCPPT_PP_DISABLE_GCC_WARNING(-Weffc++)
+
+BOOST_AUTO_TEST_CASE(
+	container_grid_map_move
+)
+{
+FCPPT_PP_POP_WARNING
+
+	typedef
+	fcppt::unique_ptr<
+		unsigned
+	>
+	uint_unique_ptr;
+
+	FCPPT_MAKE_STRONG_TYPEDEF(
+		uint_unique_ptr,
+		strong_result
+	);
+
+	typedef
+	fcppt::container::grid::object<
+		uint_unique_ptr,
+		2
+	>
+	unique_ptr_grid;
+
+	typedef
+	fcppt::container::grid::object<
+		strong_result,
+		2
+	>
+	strong_grid;
+
+
+	strong_grid const result(
+		fcppt::container::grid::map<
+			strong_grid
+		>(
+			unique_ptr_grid(
+				unique_ptr_grid::dim{
+					3u,
+					2u
+				},
+				[](
+					unique_ptr_grid::pos const _pos
+				)
+				{
+					return
+						fcppt::make_unique_ptr<
+							unsigned
+						>(
+							fcppt::cast::size<
+								unsigned
+							>(
+								fcppt::math::at_c<
+									0
+								>(
+									_pos
+								)
+								+
+								fcppt::math::at_c<
+									1
+								>(
+									_pos
+								)
+							)
+						);
+				}
+			),
+			[](
+				uint_unique_ptr &&_value
+			)
+			{
+				return
+					strong_result(
+						std::move(
+							_value
+						)
+					);
+			}
+		)
+	);
+
+	BOOST_REQUIRE_EQUAL(
+		strong_grid::dim(
+			3u,
+			2u
+		),
+		result.size()
+	);
+
+	BOOST_CHECK_EQUAL(
+		*result[
+			strong_grid::pos(
+				0u,
+				0u
+			)
+		].get(),
+		0u
+	);
+
+	BOOST_CHECK_EQUAL(
+		*result[
+			strong_grid::pos(
+				2u,
+				1u
+			)
+		].get(),
+		3u
+	);
+}
