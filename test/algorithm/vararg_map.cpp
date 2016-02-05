cde433adb4d08ac5b038e3e7b6fa33e827a589ce
Carl Philipp Reh <carlphilippreh@gmail.com>
2016-02-05 22:07:36 +0100

    Added move test for vararg_map

--- test/algorithm/vararg_map.cpp
+++ test/algorithm/vararg_map.cpp
@@ -6,6 +6,7 @@
 
 #include <fcppt/decltype_sink.hpp>
 #include <fcppt/literal.hpp>
+#include <fcppt/noncopyable.hpp>
 #include <fcppt/tag_type.hpp>
 #include <fcppt/algorithm/vararg_map.hpp>
 #include <fcppt/cast/to_unsigned.hpp>
@@ -16,6 +17,7 @@
 #include <boost/mpl/vector/vector10.hpp>
 #include <boost/test/unit_test.hpp>
 #include <tuple>
+#include <utility>
 #include <fcppt/config/external_end.hpp>
 
 
@@ -82,5 +84,96 @@ FCPPT_PP_POP_WARNING
 			result
 		),
 		42uL
+	);
+}
+
+namespace
+{
+
+class movable
+{
+	FCPPT_NONCOPYABLE(
+		movable
+	);
+public:
+	explicit
+	movable(
+		int const _value
+	)
+	:
+		value_{
+			_value
+		}
+	{
+	}
+
+	~movable()
+	{
+	}
+
+	movable(
+		movable &&
+	) = default;
+
+	int
+	value() const
+	{
+		return
+			value_;
+	}
+private:
+	int value_;
+};
+
+}
+
+FCPPT_PP_PUSH_WARNING
+FCPPT_PP_DISABLE_GCC_WARNING(-Weffc++)
+
+BOOST_AUTO_TEST_CASE(
+	vararg_map_move
+)
+{
+FCPPT_PP_POP_WARNING
+
+	movable const result{
+		fcppt::algorithm::vararg_map<
+			boost::mpl::vector1<
+				movable
+			>
+		>(
+			[](
+				auto && _args
+			){
+				return
+					movable(
+						std::forward<
+							decltype(
+								_args
+							)
+						>(
+							_args
+						)
+					);
+			},
+			[](
+				auto const _tag
+			)
+			{
+				return
+					fcppt::tag_type<
+						FCPPT_DECLTYPE_SINK(
+							_tag
+						)
+					>(
+						42
+					);
+			}
+		)
+	};
+
+	BOOST_CHECK_EQUAL(
+		result.value(),
+		42
 	);
 }
