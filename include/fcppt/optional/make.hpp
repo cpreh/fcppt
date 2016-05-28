b79f96f2f515c3d9b2fd01f9d7aa4dcbcf00a488
Carl Philipp Reh <carlphilippreh@gmail.com>
2016-05-28 11:55:22 +0200

    Added optional::make

---
+++ include/fcppt/optional/make.hpp
@@ -0,0 +1,59 @@
+//          Copyright Carl Philipp Reh 2009 - 2016.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef FCPPT_OPTIONAL_MAKE_HPP_INCLUDED
+#define FCPPT_OPTIONAL_MAKE_HPP_INCLUDED
+
+#include <fcppt/optional/object_impl.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <type_traits>
+#include <utility>
+#include <fcppt/config/external_end.hpp>
+
+
+namespace fcppt
+{
+namespace optional
+{
+
+/**
+\brief Wraps a value into an optional
+
+\ingroup fcpptoptional
+*/
+template<
+	typename Type
+>
+inline
+fcppt::optional::object<
+	typename
+	std::decay<
+		Type
+	>::type
+>
+make(
+	Type &&_value
+)
+{
+	return
+		fcppt::optional::object<
+			typename
+			std::decay<
+				Type
+			>::type
+		>{
+			std::forward<
+				Type
+			>(
+				_value
+			)
+		};
+}
+
+}
+}
+
+#endif
