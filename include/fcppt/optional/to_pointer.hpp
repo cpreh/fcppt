bb8c8dca2fd450891e809781dba1725fb7cf9a2d
Carl Philipp Reh <carlphilippreh@gmail.com>
2016-08-06 12:49:00 +0200

    Added optional::to_pointer

---
+++ include/fcppt/optional/to_pointer.hpp
@@ -0,0 +1,63 @@
+//          Copyright Carl Philipp Reh 2009 - 2016.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef FCPPT_OPTIONAL_TO_POINTER_HPP_INCLUDED
+#define FCPPT_OPTIONAL_TO_POINTER_HPP_INCLUDED
+
+#include <fcppt/reference_impl.hpp>
+#include <fcppt/optional/maybe.hpp>
+#include <fcppt/optional/reference.hpp>
+
+
+namespace fcppt
+{
+namespace optional
+{
+
+/**
+\brief Converts an optional reference to a pointer
+
+\ingroup fcpptoptional
+
+If \a _optional is empty, returns <code>nullptr</code>. Otherwise, returns the
+address of the referenced object of \a _optional.
+*/
+template<
+	typename T
+>
+inline
+T *
+to_pointer(
+	fcppt::optional::reference<
+		T
+	> const _optional
+)
+{
+	return
+		fcppt::optional::maybe(
+			_optional,
+			[]()
+			-> T *
+			{
+				return
+					nullptr;
+			},
+			[](
+				fcppt::reference<
+					T
+				> const _ref
+			)
+			{
+				return
+					_ref.get_pointer();
+			}
+		);
+}
+
+}
+}
+
+#endif
---
+++ test/optional/to_pointer.cpp
@@ -0,0 +1,56 @@
+//          Copyright Carl Philipp Reh 2009 - 2016.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <fcppt/make_cref.hpp>
+#include <fcppt/optional/reference.hpp>
+#include <fcppt/optional/to_pointer.hpp>
+#include <fcppt/preprocessor/disable_gcc_warning.hpp>
+#include <fcppt/preprocessor/pop_warning.hpp>
+#include <fcppt/preprocessor/push_warning.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <boost/test/unit_test.hpp>
+#include <fcppt/config/external_end.hpp>
+
+
+FCPPT_PP_PUSH_WARNING
+FCPPT_PP_DISABLE_GCC_WARNING(-Weffc++)
+
+BOOST_AUTO_TEST_CASE(
+	optional_to_pointer
+)
+{
+FCPPT_PP_POP_WARNING
+
+	int const val{
+		42
+	};
+
+	typedef
+	fcppt::optional::reference<
+		int const
+	>
+	optional_int_cref;
+
+	BOOST_CHECK(
+		fcppt::optional::to_pointer(
+			optional_int_cref{}
+		)
+		==
+		nullptr
+	);
+
+	BOOST_CHECK(
+		fcppt::optional::to_pointer(
+			optional_int_cref{
+				fcppt::make_cref(
+					val
+				)
+			}
+		)
+		==
+		&val
+	);
+}
