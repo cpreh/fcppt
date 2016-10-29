cc0bfd4faeea78b870381478d4902fa6d6304a85
Carl Philipp Reh <carlphilippreh@gmail.com>
2016-10-29 14:31:03 +0200

    Added reference_to_const

---
+++ libs/core/include/fcppt/reference_to_const.hpp
@@ -0,0 +1,56 @@
+//          Copyright Carl Philipp Reh 2009 - 2016.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef FCPPT_REFERENCE_TO_CONST_HPP_INCLUDED
+#define FCPPT_REFERENCE_TO_CONST_HPP_INCLUDED
+
+#include <fcppt/reference_impl.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <type_traits>
+#include <fcppt/config/external_end.hpp>
+
+
+namespace fcppt
+{
+
+/**
+\brief Converts a reference to a const reference
+
+\ingroup fcpptref
+
+\tparam Type Must not be const
+*/
+template<
+	typename Type
+>
+inline
+fcppt::reference<
+	Type const
+>
+reference_to_const(
+	fcppt::reference<
+		Type
+	> const _ref
+)
+{
+	static_assert(
+		!std::is_const<
+			Type
+		>::value,
+		"Type must not be const"
+	);
+
+	return
+		fcppt::reference<
+			Type const
+		>(
+			_ref.get()
+		);
+}
+
+}
+
+#endif
---
+++ test/reference_to_const.cpp
@@ -0,0 +1,43 @@
+//          Copyright Carl Philipp Reh 2009 - 2016.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <fcppt/make_cref.hpp>
+#include <fcppt/make_ref.hpp>
+#include <fcppt/reference_comparison.hpp>
+#include <fcppt/reference_to_const.hpp>
+#include <fcppt/preprocessor/disable_gcc_warning.hpp>
+#include <fcppt/preprocessor/pop_warning.hpp>
+#include <fcppt/preprocessor/push_warning.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <boost/test/unit_test.hpp>
+#include <fcppt/config/external_end.hpp>
+
+
+FCPPT_PP_PUSH_WARNING
+FCPPT_PP_DISABLE_GCC_WARNING(-Weffc++)
+
+BOOST_AUTO_TEST_CASE(
+	reference_to_const
+)
+{
+FCPPT_PP_POP_WARNING
+
+	int value{
+		0
+	};
+
+	BOOST_CHECK(
+		fcppt::reference_to_const(
+			fcppt::make_ref(
+				value
+			)
+		)
+		==
+		fcppt::make_cref(
+			value
+		)
+	);
+}
