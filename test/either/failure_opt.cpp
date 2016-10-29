7c8b30feabac36db72a2357540a156eaf0b17e3b
Carl Philipp Reh <carlphilippreh@gmail.com>
2016-10-29 18:47:34 +0200

    Add test for either::failure_opt

---
+++ test/either/failure_opt.cpp
@@ -0,0 +1,65 @@
+//          Copyright Carl Philipp Reh 2009 - 2016.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <fcppt/either/failure_opt.hpp>
+#include <fcppt/either/object.hpp>
+#include <fcppt/optional/object.hpp>
+#include <fcppt/optional/output.hpp>
+#include <fcppt/preprocessor/disable_gcc_warning.hpp>
+#include <fcppt/preprocessor/pop_warning.hpp>
+#include <fcppt/preprocessor/push_warning.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <boost/test/unit_test.hpp>
+#include <string>
+#include <fcppt/config/external_end.hpp>
+
+
+FCPPT_PP_PUSH_WARNING
+FCPPT_PP_DISABLE_GCC_WARNING(-Weffc++)
+
+BOOST_AUTO_TEST_CASE(
+	either_failure_opt
+)
+{
+FCPPT_PP_POP_WARNING
+
+	typedef
+	fcppt::either::object<
+		std::string,
+		int
+	>
+	either_int;
+
+	typedef
+	fcppt::optional::object<
+		std::string
+	>
+	optional_string;
+
+	BOOST_CHECK_EQUAL(
+		fcppt::either::failure_opt(
+			either_int(
+				std::string(
+					"test"
+				)
+			)
+		),
+		optional_string(
+			std::string(
+				"test"
+			)
+		)
+	);
+
+	BOOST_CHECK_EQUAL(
+		fcppt::either::failure_opt(
+			either_int(
+				42
+			)
+		),
+		optional_string()
+	);
+}
