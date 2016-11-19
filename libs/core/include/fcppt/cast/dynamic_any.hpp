8755fae3d728d2a2ac7253f83581577574fd8993
Carl Philipp Reh <carlphilippreh@gmail.com>
2016-11-19 19:39:01 +0100

    Added cast::dynamic_any{_fun}

---
+++ libs/core/include/fcppt/cast/dynamic_any.hpp
@@ -0,0 +1,54 @@
+//          Copyright Carl Philipp Reh 2009 - 2016.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef FCPPT_CAST_DYNAMIC_ANY_HPP_INCLUDED
+#define FCPPT_CAST_DYNAMIC_ANY_HPP_INCLUDED
+
+#include <fcppt/cast/detail/dynamic.hpp>
+#include <fcppt/optional/reference.hpp>
+
+
+namespace fcppt
+{
+namespace cast
+{
+
+/**
+\brief Converts between references using <code>dynamic_cast</code>, returning an empty optional on failure
+
+\ingroup fcpptcasts
+
+Tries to cast \a _src to \a Dest using <code>dynamic_cast</code>. On failure,
+an empty optional is returned.
+
+\tparam Derived The type to cast to. Can be cv-qualified.
+
+\tparam Base A cv-qualified non-reference type.
+*/
+template<
+	typename Derived,
+	typename Base
+>
+inline
+fcppt::optional::reference<
+	Derived
+>
+dynamic_any(
+	Base &_base
+)
+{
+	return
+		fcppt::cast::detail::dynamic<
+			Derived
+		>(
+			_base
+		);
+}
+
+}
+}
+
+#endif
---
+++ test/cast/dynamic_any.cpp
@@ -0,0 +1,115 @@
+//          Copyright Carl Philipp Reh 2009 - 2016.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <fcppt/noncopyable.hpp>
+#include <fcppt/cast/apply.hpp>
+#include <fcppt/cast/dynamic_any.hpp>
+#include <fcppt/cast/dynamic_any_fun.hpp>
+#include <fcppt/preprocessor/disable_gcc_warning.hpp>
+#include <fcppt/preprocessor/pop_warning.hpp>
+#include <fcppt/preprocessor/push_warning.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <boost/test/unit_test.hpp>
+#include <fcppt/config/external_end.hpp>
+
+
+namespace
+{
+
+struct base
+{
+	FCPPT_NONCOPYABLE(
+		base
+	);
+
+	base()
+	{
+	}
+
+	virtual
+	~base()
+	{
+	}
+};
+
+struct derived1
+:
+base
+{
+	FCPPT_NONCOPYABLE(
+		derived1
+	);
+
+	derived1()
+	{
+	}
+
+	~derived1()
+	override
+	{
+	}
+};
+
+struct other
+{
+	FCPPT_NONCOPYABLE(
+		other
+	);
+
+	other()
+	{
+	}
+
+	virtual
+	~other()
+	{
+	}
+};
+
+}
+
+FCPPT_PP_PUSH_WARNING
+FCPPT_PP_DISABLE_GCC_WARNING(-Weffc++)
+
+BOOST_AUTO_TEST_CASE(
+	cast_dynamic_any
+)
+{
+FCPPT_PP_POP_WARNING
+
+	derived1 d1{};
+
+	other d2{};
+
+	base &b1{
+		d1
+	};
+
+	BOOST_CHECK(
+		fcppt::cast::dynamic_any<
+			derived1
+		>(
+			b1
+		).has_value()
+	);
+
+	BOOST_CHECK((
+		fcppt::cast::apply<
+			fcppt::cast::dynamic_any_fun,
+			derived1
+		>(
+			b1
+		).has_value()
+	));
+
+	BOOST_CHECK(
+		!fcppt::cast::dynamic_any<
+			other
+		>(
+			b1
+		).has_value()
+	);
+}
