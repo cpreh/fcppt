3aedde1cb3494d1a4fb23ec84ef31f9d827916d8
Carl Philipp Reh <carlphilippreh@gmail.com>
2017-02-04 11:47:32 +0100

    Added vector/static test

---
+++ test/math/vector/static.cpp
@@ -0,0 +1,67 @@
+//          Copyright Carl Philipp Reh 2009 - 2016.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <fcppt/math/size_type.hpp>
+#include <fcppt/math/vector/static.hpp>
+#include <fcppt/preprocessor/disable_gcc_warning.hpp>
+#include <fcppt/preprocessor/pop_warning.hpp>
+#include <fcppt/preprocessor/push_warning.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <boost/test/unit_test.hpp>
+#include <fcppt/config/external_end.hpp>
+
+
+namespace
+{
+
+template<
+	typename T,
+	fcppt::math::size_type N
+>
+void
+test_func(
+	fcppt::math::vector::static_<
+		T,
+		N
+	> const _value
+)
+{
+	BOOST_CHECK_EQUAL(
+		_value[0],
+		0
+	);
+
+	BOOST_CHECK_EQUAL(
+		_value[1],
+		1
+	);
+}
+
+}
+
+FCPPT_PP_PUSH_WARNING
+FCPPT_PP_DISABLE_GCC_WARNING(-Weffc++)
+
+BOOST_AUTO_TEST_CASE(
+	math_vector_static
+)
+{
+FCPPT_PP_POP_WARNING
+
+	typedef
+	fcppt::math::vector::static_<
+		int,
+		2
+	>
+	int2_vector;
+
+	test_func(
+		int2_vector{
+			0,
+			1
+		}
+	);
+}
