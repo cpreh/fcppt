9c6a2afcd57695fdeadba07622666fbbe527e818
Carl Philipp Reh <carlphilippreh@gmail.com>
2017-02-12 12:39:49 +0100

    Added variant test for moves in apply_ternary

--- test/variant/apply_movable.cpp
+++ test/variant/apply_movable.cpp
@@ -6,6 +6,7 @@
 
 #include <fcppt/make_unique_ptr.hpp>
 #include <fcppt/unique_ptr.hpp>
+#include <fcppt/assert/unreachable.hpp>
 #include <fcppt/preprocessor/disable_gcc_warning.hpp>
 #include <fcppt/preprocessor/pop_warning.hpp>
 #include <fcppt/preprocessor/push_warning.hpp>
@@ -15,6 +16,7 @@
 #include <boost/mpl/vector/vector10.hpp>
 #include <boost/test/unit_test.hpp>
 #include <string>
+#include <tuple>
 #include <utility>
 #include <fcppt/config/external_end.hpp>
 
@@ -74,6 +76,63 @@ struct function
 	}
 };
 
+typedef
+std::tuple<
+	int_unique_ptr,
+	string_unique_ptr,
+	bool_unique_ptr
+>
+move_result;
+
+struct move_function
+{
+	template<
+		typename T1,
+		typename T2,
+		typename T3
+	>
+	move_result
+	operator()(
+		T1 &&,
+		T2 &&,
+		T3 &&
+	) const
+	{
+		FCPPT_ASSERT_UNREACHABLE;
+	}
+
+	move_result
+	operator()(
+		int_unique_ptr &&_int,
+		string_unique_ptr &&_string,
+		bool_unique_ptr &&_bool
+	) const
+	{
+		return
+			move_result{
+				std::move(
+					_int
+				),
+				std::move(
+					_string
+				),
+				std::move(
+					_bool
+				)
+			};
+	}
+};
+
+typedef
+fcppt::variant::object<
+	boost::mpl::vector3<
+		int_unique_ptr,
+		string_unique_ptr,
+		bool_unique_ptr
+	>
+>
+variant;
+
 }
 
 FCPPT_PP_PUSH_WARNING
@@ -84,16 +143,6 @@ BOOST_AUTO_TEST_CASE(
 )
 {
 FCPPT_PP_POP_WARNING
-
-	typedef
-	fcppt::variant::object<
-		boost::mpl::vector3<
-			int_unique_ptr,
-			string_unique_ptr,
-			bool_unique_ptr
-		>
-	>
-	variant;
 
 	variant int_variant(
 		fcppt::make_unique_ptr<
@@ -127,6 +176,69 @@ FCPPT_PP_POP_WARNING
 			int_variant,
 			string_variant,
 			bool_variant
+		)
+	);
+}
+
+FCPPT_PP_PUSH_WARNING
+FCPPT_PP_DISABLE_GCC_WARNING(-Weffc++)
+
+BOOST_AUTO_TEST_CASE(
+	variant_apply_move
+)
+{
+FCPPT_PP_POP_WARNING
+
+	move_result const result{
+		fcppt::variant::apply_ternary(
+			move_function{},
+			variant{
+				fcppt::make_unique_ptr<
+					int
+				>(
+					42
+				)
+			},
+			variant{
+				fcppt::make_unique_ptr<
+					std::string
+				>(
+					"test"
+				)
+			},
+			variant{
+				fcppt::make_unique_ptr<
+					bool
+				>(
+					true
+				)
+			}
+		)
+	};
+
+	BOOST_CHECK_EQUAL(
+		*std::get<
+			0
+		>(
+			result
+		),
+		42
+	);
+
+	BOOST_CHECK_EQUAL(
+		*std::get<
+			1
+		>(
+			result
+		),
+		"test"
+	);
+
+	BOOST_CHECK(
+		*std::get<
+			2
+		>(
+			result
 		)
 	);
 }
