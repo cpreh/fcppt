c1e3de747648332817824eb87a3a107a66bbc684
Carl Philipp Reh <carlphilippreh@gmail.com>
2017-05-20 16:52:57 +0200

    Added either::fold_error

---
+++ libs/core/include/fcppt/either/fold_error.hpp
@@ -0,0 +1,162 @@
+//          Copyright Carl Philipp Reh 2009 - 2016.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef FCPPT_EITHER_FOLD_ERROR_HPP_INCLUDED
+#define FCPPT_EITHER_FOLD_ERROR_HPP_INCLUDED
+
+#include <fcppt/loop.hpp>
+#include <fcppt/move_if_rvalue.hpp>
+#include <fcppt/algorithm/fold_break.hpp>
+#include <fcppt/container/to_value_type.hpp>
+#include <fcppt/either/is_object.hpp>
+#include <fcppt/either/match.hpp>
+#include <fcppt/either/no_error.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <type_traits>
+#include <utility>
+#include <fcppt/config/external_end.hpp>
+
+
+namespace fcppt
+{
+namespace either
+{
+
+/**
+\brief Folds over a range, breaking out on the first error.
+
+\ingroup fcppteither
+
+This function is similar to \link fcppt::either::sequence\endlink, except it
+does not produce an output sequence. Instead, \a _function returns \link
+fcppt::either::error\endlink, which on success contains an \link
+fcppt::unit\endlink on success.
+Let <code>_sequence = [x_1, ..., x_n]</code>.
+The algorithms calls <code>_function(x_1), ..., _function(x_i)</code>,
+where <code>_function(x_i)</code> is either the first call that returns a failure, in
+which case the failure is returned as the result, or <code>i=n</code>, in which case
+success is returned.
+
+\tparam Sequence Must be a range.
+
+\tparam Function Must be a function callable as
+<code>fcppt::either::error<T>(Sequence::value_type)</code> for some type <code>T</code>.
+*/
+template<
+	typename Sequence,
+	typename Function
+>
+auto
+fold_error(
+	Sequence &&_sequence,
+	Function const &_function
+)
+->
+decltype(
+	_function(
+		std::declval<
+			fcppt::container::to_value_type<
+				typename
+				std::remove_reference<
+					Sequence
+				>::type
+			>
+		>()
+	)
+)
+{
+	typedef
+	decltype(
+		_function(
+			std::declval<
+				fcppt::container::to_value_type<
+					typename
+					std::remove_reference<
+						Sequence
+					>::type
+				>
+			>()
+		)
+	)
+	either_type;
+
+	static_assert(
+		fcppt::either::is_object<
+			either_type
+		>::value,
+		"Function must return an either"
+	);
+
+	static_assert(
+		std::is_same<
+			typename
+			either_type::success,
+			fcppt::either::no_error
+		>::value,
+		"Function must return an either::error"
+	);
+
+	return
+		fcppt::algorithm::fold_break(
+			std::forward<
+				Sequence
+			>(
+				_sequence
+			),
+			either_type{
+				fcppt::either::no_error{}
+			},
+			[
+				&_function
+			](
+				auto &&_element,
+				either_type
+			)
+			{
+				return
+					fcppt::either::match(
+						_function(
+							fcppt::move_if_rvalue<
+								Sequence
+							>(
+								_element
+							)
+						),
+						[](
+							typename
+							either_type::failure &&_error
+						)
+						{
+							return
+								std::make_pair(
+									fcppt::loop::break_,
+									either_type{
+										std::move(
+											_error
+										)
+									}
+								);
+						},
+						[](
+							fcppt::either::no_error
+						){
+							return
+								std::make_pair(
+									fcppt::loop::continue_,
+									either_type{
+										fcppt::either::no_error{}
+									}
+								);
+						}
+					);
+			}
+		);
+}
+
+}
+}
+
+#endif
---
+++ test/either/fold_error.cpp
@@ -0,0 +1,132 @@
+//          Copyright Carl Philipp Reh 2009 - 2016.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <fcppt/make_unique_ptr.hpp>
+#include <fcppt/unique_ptr_impl.hpp>
+#include <fcppt/unit_comparison.hpp>
+#include <fcppt/unit_output.hpp>
+#include <fcppt/assign/make_container.hpp>
+#include <fcppt/either/comparison.hpp>
+#include <fcppt/either/error.hpp>
+#include <fcppt/either/fold_error.hpp>
+#include <fcppt/either/no_error.hpp>
+#include <fcppt/either/output.hpp>
+#include <fcppt/preprocessor/disable_gcc_warning.hpp>
+#include <fcppt/preprocessor/pop_warning.hpp>
+#include <fcppt/preprocessor/push_warning.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <boost/test/unit_test.hpp>
+#include <string>
+#include <vector>
+#include <fcppt/config/external_end.hpp>
+
+
+FCPPT_PP_PUSH_WARNING
+FCPPT_PP_DISABLE_GCC_WARNING(-Weffc++)
+
+BOOST_AUTO_TEST_CASE(
+	either_fold_error
+)
+{
+FCPPT_PP_POP_WARNING
+
+	typedef
+	fcppt::either::error<
+		std::string
+	>
+	either_error;
+
+	{
+		either_error const result{
+			fcppt::either::fold_error(
+				std::vector<
+					int
+				>{
+					1,2,3
+				},
+				[](
+					int const _value
+				)
+				{
+					return
+						_value % 2
+						==
+						0
+						?
+							either_error{
+								std::string{"test"}
+							}
+						:
+							either_error{
+								fcppt::either::no_error{}
+							}
+						;
+				}
+			)
+		};
+
+		BOOST_CHECK_EQUAL(
+			result,
+			either_error{
+				std::string{
+					"test"
+				}
+			}
+		);
+	}
+
+	{
+		either_error const result{
+			fcppt::either::fold_error(
+				fcppt::assign::make_container<
+					std::vector<
+						fcppt::unique_ptr<
+							int
+						>
+					>
+				>(
+					fcppt::make_unique_ptr<
+						int
+					>(
+						2
+					),
+					fcppt::make_unique_ptr<
+						int
+					>(
+						4
+					)
+				),
+				[](
+					fcppt::unique_ptr<
+						int
+					> &&_value
+				)
+				{
+					return
+						*_value % 2
+						==
+						0
+						?
+							either_error{
+								fcppt::either::no_error{}
+							}
+						:
+							either_error{
+								std::string{"test"}
+							}
+						;
+				}
+			)
+		};
+
+		BOOST_CHECK_EQUAL(
+			result,
+			either_error{
+				fcppt::either::no_error{}
+			}
+		);
+	}
+}
