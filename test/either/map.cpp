8d6784419717f2126be6e8f12cc263f10cfa8c53
Carl Philipp Reh <carlphilippreh@gmail.com>
2017-06-04 18:42:04 +0200

    Added either::map move test

--- test/either/map.cpp
+++ test/either/map.cpp
@@ -4,7 +4,11 @@
 //          http://www.boost.org/LICENSE_1_0.txt)
 
 
+#include <fcppt/make_unique_ptr.hpp>
+#include <fcppt/strong_typedef.hpp>
+#include <fcppt/unique_ptr.hpp>
 #include <fcppt/either/map.hpp>
+#include <fcppt/either/match.hpp>
 #include <fcppt/either/object.hpp>
 #include <fcppt/either/output.hpp>
 #include <fcppt/preprocessor/disable_gcc_warning.hpp>
@@ -13,6 +17,7 @@
 #include <fcppt/config/external_begin.hpp>
 #include <boost/test/unit_test.hpp>
 #include <string>
+#include <utility>
 #include <fcppt/config/external_end.hpp>
 
 
@@ -76,6 +81,79 @@ FCPPT_PP_POP_WARNING
 		),
 		either_bool(
 			true
+		)
+	);
+}
+
+FCPPT_PP_PUSH_WARNING
+FCPPT_PP_DISABLE_GCC_WARNING(-Weffc++)
+
+BOOST_AUTO_TEST_CASE(
+	either_map_move
+)
+{
+FCPPT_PP_POP_WARNING
+
+	typedef
+	fcppt::either::object<
+		std::string,
+		fcppt::unique_ptr<
+			int
+		>
+	>
+	either_int;
+
+	FCPPT_MAKE_STRONG_TYPEDEF(
+		fcppt::unique_ptr<
+			int
+		>,
+		strong_int
+	);
+
+	auto const map_function(
+		[](
+			fcppt::unique_ptr<
+				int
+			> &&_ptr
+		)
+		{
+			return
+				strong_int{
+					std::move(
+						_ptr
+					)
+				};
+		}
+	);
+
+	BOOST_CHECK(
+		fcppt::either::match(
+			fcppt::either::map(
+				either_int(
+					fcppt::make_unique_ptr<
+						int
+					>(
+						20
+					)
+				),
+				map_function
+			),
+			[](
+				std::string const &
+			)
+			{
+				return
+					false;
+			},
+			[](
+				strong_int const &_value
+			)
+			{
+				return
+					*_value.get()
+					==
+					20;
+			}
 		)
 	);
 }
