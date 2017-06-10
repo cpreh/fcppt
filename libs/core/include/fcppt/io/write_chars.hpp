e404fba071112dda9b4d17bf19a0cdb1382ee300
Carl Philipp Reh <carlphilippreh@gmail.com>
2017-06-10 11:00:18 +0200

    Added io::write_chars

---
+++ libs/core/include/fcppt/io/write_chars.hpp
@@ -0,0 +1,44 @@
+//          Copyright Carl Philipp Reh 2009 - 2016.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef FCPPT_IO_WRITE_CHARS_HPP_INCLUDED
+#define FCPPT_IO_WRITE_CHARS_HPP_INCLUDED
+
+#include <fcppt/detail/symbol.hpp>
+#include <fcppt/preprocessor/warn_unused_result.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <cstddef>
+#include <iosfwd>
+#include <fcppt/config/external_end.hpp>
+
+
+namespace fcppt
+{
+namespace io
+{
+
+/**
+\brief Writes a number of chars
+
+Tries to write \a count chars from \a data to \a stream.
+
+\ingroup fcpptio
+
+\return If the write operation succeeded.
+*/
+FCPPT_DETAIL_SYMBOL
+bool
+write_chars(
+	std::ostream &stream,
+	char const *data,
+	std::size_t count
+)
+FCPPT_PP_WARN_UNUSED_RESULT;
+
+}
+}
+
+#endif
---
+++ libs/core/src/io/write_chars.cpp
@@ -0,0 +1,37 @@
+//          Copyright Carl Philipp Reh 2009 - 2016.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <fcppt/cast/size.hpp>
+#include <fcppt/cast/to_signed.hpp>
+#include <fcppt/io/write_chars.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <cstddef>
+#include <ios>
+#include <ostream>
+#include <fcppt/config/external_end.hpp>
+
+
+bool
+fcppt::io::write_chars(
+	std::ostream &_stream,
+	char const *const _data,
+	std::size_t const _count
+)
+{
+	_stream.write(
+		_data,
+		fcppt::cast::size<
+			std::streamsize
+		>(
+			fcppt::cast::to_signed(
+				_count
+			)
+		)
+	);
+
+	return
+		_stream.good();
+}
