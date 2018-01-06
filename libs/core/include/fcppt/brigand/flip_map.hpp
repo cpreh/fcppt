8a8c992ab0c502fb46e81f4d19de1996fe99957a
Carl Philipp Reh <carlphilippreh@gmail.com>
2018-01-06 17:01:11 +0100

    Added brigand::flip_pair and brigand::flip_map

---
+++ libs/core/include/fcppt/brigand/flip_map.hpp
@@ -0,0 +1,51 @@
+//          Copyright Carl Philipp Reh 2009 - 2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef FCPPT_BRIGAND_FLIP_MAP_HPP_INCLUDED
+#define FCPPT_BRIGAND_FLIP_MAP_HPP_INCLUDED
+
+#include <fcppt/brigand/flip_pair.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <brigand/algorithms/transform.hpp>
+#include <brigand/functions/lambda/bind.hpp>
+#include <brigand/types/args.hpp>
+#include <fcppt/config/external_end.hpp>
+
+
+namespace fcppt
+{
+namespace brigand
+{
+
+/**
+\brief Flips a brigand map.
+
+\ingroup fcpptbrigand
+
+For each element of \a Map, swaps the key and the value type.
+For this to work, the mapped types of the map must all be different ypes.
+
+\tparam Map A brigand map.
+*/
+template<
+	typename Map
+>
+using
+flip_map
+=
+::brigand::transform<
+	Map,
+	::brigand::bind<
+		fcppt::brigand::flip_pair,
+		::brigand::_1
+	>
+>;
+
+}
+}
+
+#endif
+
---
+++ test/brigand/flip_map.cpp
@@ -0,0 +1,45 @@
+//          Copyright Carl Philipp Reh 2009 - 2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <fcppt/brigand/flip_map.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <brigand/sequences/map.hpp>
+#include <brigand/sequences/pair.hpp>
+#include <type_traits>
+#include <fcppt/config/external_end.hpp>
+
+
+int
+main()
+{
+	static_assert(
+		std::is_same<
+			fcppt::brigand::flip_map<
+				brigand::map<
+					brigand::pair<
+						int,
+						float
+					>,
+					brigand::pair<
+						char,
+						long
+					>
+				>
+			>,
+			brigand::map<
+				brigand::pair<
+					float,
+					int
+				>,
+				brigand::pair<
+					long,
+					char
+				>
+			>
+		>::value,
+		""
+	);
+}
