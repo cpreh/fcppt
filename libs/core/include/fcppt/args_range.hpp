e93cdf23bc376d88c2af613a5441825f1f6b0a64
Carl Philipp Reh <carlphilippreh@gmail.com>
2018-04-21 11:27:28 +0200

    Added args_range

---
+++ libs/core/include/fcppt/args_range.hpp
@@ -0,0 +1,14 @@
+//          Copyright Carl Philipp Reh 2009 - 2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef FCPPT_ARGS_RANGE_HPP_INCLUDED
+#define FCPPT_ARGS_RANGE_HPP_INCLUDED
+
+#include <fcppt/args_range_fwd.hpp>
+#include <fcppt/iterator/range_impl.hpp>
+
+
+#endif
---
+++ libs/core/include/fcppt/args_range_fwd.hpp
@@ -0,0 +1,30 @@
+//          Copyright Carl Philipp Reh 2009 - 2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef FCPPT_ARGS_RANGE_FWD_HPP_INCLUDED
+#define FCPPT_ARGS_RANGE_FWD_HPP_INCLUDED
+
+#include <fcppt/args_vector.hpp>
+#include <fcppt/iterator/range_fwd.hpp>
+
+
+namespace fcppt
+{
+
+/**
+\brief A view over command line arguments
+
+\ingroup fcpptvarious
+*/
+typedef
+fcppt::iterator::range<
+	fcppt::args_vector::const_iterator
+>
+args_range;
+
+}
+
+#endif
