2008d552417ef9378e3989881eb139275cc682d4
Carl Philipp Reh <carlphilippreh@gmail.com>
2018-09-30 11:45:05 +0200

    Use catch::movable in strong_typedef/map test

--- test/strong_typedef/map.cpp
+++ test/strong_typedef/map.cpp
@@ -6,12 +6,12 @@
 
 #include <fcppt/make_strong_typedef.hpp>
 #include <fcppt/make_unique_ptr.hpp>
-#include <fcppt/noncopyable.hpp>
 #include <fcppt/output_to_std_string.hpp>
 #include <fcppt/strong_typedef_impl.hpp>
 #include <fcppt/strong_typedef_map.hpp>
 #include <fcppt/strong_typedef_tag.hpp>
 #include <fcppt/unique_ptr_impl.hpp>
+#include <fcppt/catch/movable.hpp>
 #include <fcppt/config/external_begin.hpp>
 #include <catch2/catch.hpp>
 #include <string>
@@ -38,39 +38,11 @@ FCPPT_MAKE_STRONG_TYPEDEF(
 	strong_int_ptr
 );
 
-class move_test
-{
-	FCPPT_NONCOPYABLE(
-		move_test
-	);
-public:
-	explicit
-	move_test(
-		int_unique_ptr &&_ptr
-	)
-	:
-		ptr_{
-			std::move(
-				_ptr
-			)
-		}
-	{
-	}
-
-	move_test(
-		move_test &&
-	) = default;
-
-	move_test &
-	operator=(
-		move_test &&
-	) = default;
-
-	~move_test()
-	{}
-
-	int_unique_ptr ptr_;
-};
+typedef
+fcppt::catch_::movable<
+	int_unique_ptr
+>
+move_test;
 
 }
 
@@ -142,7 +114,7 @@ TEST_CASE(
 	};
 
 	CHECK(
-		*result.get().ptr_
+		*result.get().value()
 		==
 		5
 	);
