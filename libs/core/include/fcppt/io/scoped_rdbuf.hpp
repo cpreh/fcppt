456409c6ed4501f38a15e14c219694b40fd51712
Carl Philipp Reh <carlphilippreh@gmail.com>
2018-10-14 09:37:54 +0200

    Remove explicit instantiation of scoped_rdbuf

--- libs/core/include/fcppt/io/scoped_rdbuf.hpp
+++ libs/core/include/fcppt/io/scoped_rdbuf.hpp
@@ -7,16 +7,8 @@
 #ifndef FCPPT_IO_SCOPED_RDBUF_HPP_INCLUDED
 #define FCPPT_IO_SCOPED_RDBUF_HPP_INCLUDED
 
-#include <fcppt/char_type.hpp>
-#include <fcppt/io/basic_scoped_rdbuf_decl.hpp>
+#include <fcppt/io/basic_scoped_rdbuf_impl.hpp>
 #include <fcppt/io/scoped_rdbuf_fwd.hpp>
 
-
-extern
-template
-class
-fcppt::io::basic_scoped_rdbuf<
-	fcppt::char_type
->;
 
 #endif
