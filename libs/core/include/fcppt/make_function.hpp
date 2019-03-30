ad66a4301280e1fbc4c01f0b11b270f81ff00438
Carl Philipp Reh <carlphilippreh@gmail.com>
2019-03-30 11:21:41 +0100

    Added make_function

---
+++ libs/core/include/fcppt/make_function.hpp
@@ -0,0 +1,50 @@
+//          Copyright Carl Philipp Reh 2009 - 2018.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef FCPPT_MAKE_FUNCTION_HPP_INCLUDED
+#define FCPPT_MAKE_FUNCTION_HPP_INCLUDED
+
+#include <fcppt/function_impl.hpp>
+
+
+namespace fcppt
+{
+
+/**
+\brief Creates an \link fcppt::function\endlink.
+
+\ingroup fcpptvarious
+*/
+template<
+	typename Ret,
+	typename... Args
+>
+fcppt::function<
+	Ret (
+		Args...
+	)
+>
+make_function(
+	Ret (
+		&_function
+	)(
+		Args...
+	)
+)
+{
+	return
+		fcppt::function<
+			Ret (
+				Args...
+			)
+		>(
+			_function
+		);
+}
+
+}
+
+#endif
---
+++ test/make_function.cpp
@@ -0,0 +1,54 @@
+//          Copyright Carl Philipp Reh 2009 - 2018.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <fcppt/function.hpp>
+#include <fcppt/make_function.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <catch2/catch.hpp>
+#include <fcppt/config/external_end.hpp>
+
+
+namespace
+{
+
+int
+function(
+	int const _value
+)
+{
+	return
+		_value + 1;
+}
+
+}
+
+TEST_CASE(
+	"make_function",
+	"[various]"
+)
+{
+	typedef
+	fcppt::function<
+		int (
+			int
+		)
+	>
+	int_function;
+
+	int_function const func{
+		fcppt::make_function(
+			function
+		)
+	};
+
+	CHECK(
+		func(
+			10
+		)
+		==
+		11
+	);
+}
