1acc32f48969cf9fa1f220a6a3072a82195c285b
Carl Philipp Reh <carlphilippreh@gmail.com>
2019-05-19 11:24:18 +0200

    Use operator== in bitfield operators test

--- test/container/bitfield/operators.cpp
+++ test/container/bitfield/operators.cpp
@@ -4,36 +4,86 @@
 //          http://www.boost.org/LICENSE_1_0.txt)
 
 
+#include <fcppt/text.hpp>
 #include <fcppt/container/bitfield/comparison.hpp>
 #include <fcppt/container/bitfield/enum_object.hpp>
 #include <fcppt/container/bitfield/operators.hpp>
+#include <fcppt/container/bitfield/output.hpp>
+#include <fcppt/enum/names_array.hpp>
+#include <fcppt/enum/names_impl_fwd.hpp>
+#include <fcppt/preprocessor/disable_clang_warning.hpp>
+#include <fcppt/preprocessor/pop_warning.hpp>
+#include <fcppt/preprocessor/push_warning.hpp>
 #include <fcppt/config/external_begin.hpp>
 #include <catch2/catch.hpp>
 #include <fcppt/config/external_end.hpp>
 
+
+namespace
+{
+
+enum class test_enum
+{
+	test1,
+	test2,
+	test3,
+	fcppt_maximum = test3
+};
+
+FCPPT_PP_PUSH_WARNING
+FCPPT_PP_DISABLE_CLANG_WARNING(-Wglobal-constructors)
+FCPPT_PP_DISABLE_CLANG_WARNING(-Wexit-time-destructors)
+
+fcppt::enum_::names_array<
+	test_enum
+> const names{{{
+	FCPPT_TEXT("test1"),
+	FCPPT_TEXT("test2"),
+	FCPPT_TEXT("test3")
+}}};
+
+FCPPT_PP_POP_WARNING
+
+}
+
+namespace fcppt
+{
+namespace enum_
+{
+
+template<>
+struct names_impl<
+	test_enum
+>
+{
+	static
+	fcppt::enum_::names_array<
+		test_enum
+	> const &
+	get()
+	{
+		return
+			::names;
+	}
+};
+
+}
+}
 
 TEST_CASE(
 	"container::bitfield operators",
 	"[container],[bitfield]"
 )
 {
-	enum class test_enum
-	{
-		test1,
-		test2,
-		test3,
-		fcppt_maximum = test3
-	};
-
 	typedef
 	fcppt::container::bitfield::enum_object<
 		test_enum
 	>
 	bitfield;
 
-	bitfield field1(
+	bitfield field1{
 		bitfield::null()
-	);
+	};
 
 	field1[
 		test_enum::test1
@@ -53,58 +103,27 @@ TEST_CASE(
 		test_enum::test3
 	] = true;
 
-	{
-		bitfield const bf_or(
-			field1 | field2
-		);
+	CHECK(
+		(field1 | field2)
+		==
+		bitfield{
+			test_enum::test1,
+			test_enum::test3
+		}
+	);
 
-		CHECK(
-			bf_or[
-				test_enum::test1
-			]
-		);
-
-		CHECK_FALSE(
-			bf_or[
-				test_enum::test2
-			]
-		);
-
-		CHECK(
-			bf_or[
-				test_enum::test3
-			]
-		);
-	}
-
-	// TODO: Add operator<< to bitfield
 	CHECK(
 		(field1 & field2)
 		==
 		bitfield::null()
 	);
 
-	{
-		bitfield const bf_xor(
-			field1 ^ field2
-		);
-
-		CHECK(
-			bf_xor[
-				test_enum::test1
-			]
-		);
-
-		CHECK_FALSE(
-			bf_xor[
-				test_enum::test2
-			]
-		);
-
-		CHECK(
-			bf_xor[
-				test_enum::test3
-			]
-		);
-	}
+	CHECK(
+		(field1 ^ field2)
+		==
+		bitfield{
+			test_enum::test1,
+			test_enum::test3
+		}
+	);
 }
