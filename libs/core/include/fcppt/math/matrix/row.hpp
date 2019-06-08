47dad5bdb7a50288056b6b736f8395f67532529c
Carl Philipp Reh <carlphilippreh@gmail.com>
2019-06-08 14:58:44 +0200

    Check that all types to matrix::row are the same

--- libs/core/include/fcppt/math/matrix/row.hpp
+++ libs/core/include/fcppt/math/matrix/row.hpp
@@ -7,9 +7,15 @@
 #ifndef FCPPT_MATH_MATRIX_ROW_HPP_INCLUDED
 #define FCPPT_MATH_MATRIX_ROW_HPP_INCLUDED
 
+#include <fcppt/brigand/all_of.hpp>
 #include <fcppt/cast/size.hpp>
 #include <fcppt/math/size_type.hpp>
 #include <fcppt/math/matrix/row_type.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <brigand/sequences/list.hpp>
+#include <brigand/types/args.hpp>
+#include <type_traits>
+#include <fcppt/config/external_end.hpp>
 
 
 namespace fcppt
@@ -35,6 +41,19 @@ row(
 	Args const & ..._args
 )
 {
+	static_assert(
+		fcppt::brigand::all_of<
+			::brigand::list<
+				Args...
+			>,
+			std::is_same<
+				::brigand::_1,
+				Type
+			>
+		>::value,
+		"All row elements must have the same type"
+	);
+
 	return
 		fcppt::math::matrix::row_type<
 			Type,
