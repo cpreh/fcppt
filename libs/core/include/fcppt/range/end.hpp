0c08ba363d0f2870bfcd595a81d33a3ae2c473ac
Carl Philipp Reh <carlphilippreh@gmail.com>
2019-08-31 11:34:27 +0200

    Added range::begin/end

---
+++ libs/core/include/fcppt/range/end.hpp
@@ -0,0 +1,44 @@
+//          Copyright Carl Philipp Reh 2009 - 2018.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef FCPPT_RANGE_END_HPP_INCLUDED
+#define FCPPT_RANGE_END_HPP_INCLUDED
+
+#include <fcppt/config/external_begin.hpp>
+#include <iterator>
+#include <fcppt/config/external_end.hpp>
+
+
+namespace fcppt
+{
+namespace range
+{
+
+/**
+\brief Calls end via ADL.
+
+\ingroup fcpptrange
+*/
+template<
+	typename Range
+>
+auto
+end(
+	Range &_range
+)
+{
+	using std::end;
+
+	return
+		end(
+			_range
+		);
+}
+
+}
+}
+
+#endif
---
+++ test/range/end.cpp
@@ -0,0 +1,27 @@
+//          Copyright Carl Philipp Reh 2009 - 2018.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <fcppt/range/end.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <catch2/catch.hpp>
+#include <fcppt/config/external_end.hpp>
+
+
+TEST_CASE(
+	"range::end",
+	"[range]"
+)
+{
+	int array[3] = { 0, 0, 0 };
+
+	CHECK(
+		fcppt::range::end(
+			array
+		)
+		==
+		&array[0] + 3
+	);
+}
