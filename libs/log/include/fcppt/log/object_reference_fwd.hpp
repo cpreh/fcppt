2d67cff5f0a0e2d8ae433cfb34275a7b1f34c02e
Carl Philipp Reh <carlphilippreh@gmail.com>
2020-04-05 11:20:07 +0200

    Added log::object_reference

---
+++ libs/log/include/fcppt/log/object_reference_fwd.hpp
@@ -0,0 +1,29 @@
+//          Copyright Carl Philipp Reh 2009 - 2020.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef FCPPT_LOG_OBJECT_REFERENCE_FWD_HPP_INCLUDED
+#define FCPPT_LOG_OBJECT_REFERENCE_FWD_HPP_INCLUDED
+
+#include <fcppt/reference_fwd.hpp>
+#include <fcppt/log/object_fwd.hpp>
+
+
+namespace fcppt
+{
+namespace log
+{
+
+using
+object_reference
+=
+fcppt::reference<
+	fcppt::log::object
+>;
+
+}
+}
+
+#endif
