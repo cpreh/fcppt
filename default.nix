83bcac9e69ce7241531348de94b29b4cad425df7
Carl Philipp Reh <carlphilippreh@gmail.com>
2020-04-17 23:25:46 +0200

    Use pkgs.nix

--- default.nix
+++ default.nix
@@ -1,30 +1,12 @@
 let pkgs = import <nixpkgs> {};
     stdenv = pkgs.gcc9Stdenv;
-    metal = stdenv.mkDerivation rec {
-      pname = "metal";
-      version = "2.1.1";
-
-      src = pkgs.fetchFromGitHub {
-        owner = "brunocodutra";
-        repo = "metal";
-        rev = "v${version}";
-        sha256 = "07n1aqyaixbd66l24km5ip3pkmidkx9m3saygf7cfp6vvbgmi42l";
-      };
-
-      nativeBuildInputs = [ pkgs.cmake ];
-      buildInputs = [  ];
-
-      cmakeFlags = [ ];
-
-      enableParallelBuilding = true;
-    };
 in stdenv.mkDerivation rec {
   name = "fcppt-master";
 
   src = ./.;
 
   nativeBuildInputs = [ pkgs.cmake ];
-  buildInputs = [ pkgs.boost pkgs.catch2 metal ];
+  buildInputs = [ pkgs.boost pkgs.catch2 pkgs.metal ];
 
   cmakeFlags = [ "-DCMAKE_SKIP_BUILD_RPATH=false" "-DENABLE_BOOST=true" "-DENABLE_EXAMPLES=true" "-DENABLE_CATCH=true" "-DENABLE_TEST=true" ];
 
