a80d199e987687ee9399497489cb58c6161e1366
Carl Philipp Reh <carlphilippreh@gmail.com>
2020-12-23 13:13:36 +0100

    Add catch tuple output

---
+++ libs/catch/include/fcppt/catch/tuple.hpp
@@ -0,0 +1,34 @@
+//          Copyright Carl Philipp Reh 2009 - 2020.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef FCPPT_CATCH_TUPLE_HPP_INCLUDED
+#define FCPPT_CATCH_TUPLE_HPP_INCLUDED
+
+#include <fcppt/catch/convert.hpp>
+#include <fcppt/tuple/object_impl.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <catch2/catch.hpp>
+#include <string>
+#include <fcppt/config/external_end.hpp>
+
+namespace Catch
+{
+/**
+\brief Output specialization for #fcppt::optional::object.
+
+\ingroup fcpptcatch
+*/
+template <typename... Types>
+struct StringMaker<fcppt::tuple::object<Types...>>
+{
+  static std::string convert(fcppt::tuple::object<Types...> const &_value)
+  {
+    return fcppt::catch_::convert(_value.impl());
+  }
+};
+
+}
+
+#endif
--- test/catch/tuple.cpp
+++ test/catch/tuple.cpp
@@ -3,18 +3,19 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
+#define CATCH_CONFIG_ENABLE_TUPLE_STRINGMAKER
+
+#include <fcppt/catch/convert.hpp>
 #include <fcppt/catch/tuple.hpp>
 #include <fcppt/tuple/make.hpp>
-#include <fcppt/tuple/object.hpp>
 #include <fcppt/config/external_begin.hpp>
 #include <catch2/catch.hpp>
 #include <string>
 #include <fcppt/config/external_end.hpp>
 
-TEST_CASE("tuple::make", "[tuple]")
+TEST_CASE("catch::optional", "[catch]")
 {
   CHECK(
-  fcppt::tuple::make(4,std::string{"test"})
-  ==
-  fcppt::tuple::object<int,std::string>{4,std::string{"test"}});
+      fcppt::catch_::convert(fcppt::tuple::make(10)) ==
+      std::string{"{ 10 }"});
 }
