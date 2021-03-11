49604fbf890838bc7813f9a44f43f647e037865a
Carl Philipp Reh <carlphilippreh@gmail.com>
2021-03-11 10:27:27 +0100

    Added catch::array test

---
+++ test/catch/array.cpp
@@ -0,0 +1,18 @@
+//          Copyright Carl Philipp Reh 2009 - 2020.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#include <fcppt/array/make.hpp>
+#include <fcppt/catch/convert.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <catch2/catch.hpp>
+#include <string>
+#include <fcppt/config/external_end.hpp>
+
+TEST_CASE("catch::array", "[catch]")
+{
+  CHECK(
+      fcppt::catch_::convert(fcppt::array::make(10,10)) ==
+      std::string{"{ 10, 10 }"});
+}
