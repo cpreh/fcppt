e449396204852ad963cd7a239a977424dd64ec43
Carl Philipp Reh <carlphilippreh@gmail.com>
2021-03-11 13:54:31 +0100

    Added tuple output

---
+++ libs/core/include/fcppt/tuple/output.hpp
@@ -0,0 +1,53 @@
+//          Copyright Carl Philipp Reh 2009 - 2020.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef FCPPT_TUPLE_OUTPUT_HPP_INCLUDED
+#define FCPPT_TUPLE_OUTPUT_HPP_INCLUDED
+
+#include <fcppt/not.hpp>
+#include <fcppt/tag_type.hpp>
+#include <fcppt/use.hpp>
+#include <fcppt/algorithm/loop.hpp>
+#include <fcppt/algorithm/loop_break_metal.hpp>
+#include <fcppt/metal/interval.hpp>
+#include <fcppt/tuple/get.hpp>
+#include <fcppt/tuple/object_impl.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <metal.hpp>
+#include <cstddef>
+#include <ostream>
+#include <fcppt/config/external_end.hpp>
+
+namespace fcppt::tuple
+{
+/**
+\brief Outputs an tuple to a stream
+
+\ingroup fcppttuple
+*/
+template <typename Ch, typename Traits, typename... Types>
+std::basic_ostream<Ch, Traits> &
+operator<<(std::basic_ostream<Ch, Traits> &_stream, fcppt::tuple::object<Types...> const &_tuple)
+{
+  _stream << _stream.widen('(');
+
+  fcppt::algorithm::loop(
+      fcppt::metal::interval<std::size_t, 0U, sizeof...(Types)>{},
+      [&_stream, &_tuple](auto const _index) {
+        FCPPT_USE(_index);
+        using index = fcppt::tag_type<decltype(_index)>;
+
+        _stream << fcppt::tuple::get<index::value>(_tuple);
+        if constexpr (index::value != sizeof...(Types) - 1U)
+        {
+          _stream << _stream.widen(',');
+        }
+      });
+
+  return _stream << _stream.widen(')');
+}
+}
+
+#endif
---
+++ test/tuple/output.cpp
@@ -0,0 +1,16 @@
+//          Copyright Carl Philipp Reh 2009 - 2020.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#include <fcppt/output_to_std_string.hpp>
+#include <fcppt/tuple/make.hpp>
+#include <fcppt/tuple/output.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <catch2/catch.hpp>
+#include <fcppt/config/external_end.hpp>
+
+TEST_CASE("tuple output", "[tuple]")
+{
+  CHECK(fcppt::output_to_std_string(fcppt::tuple::make(5,10)) == "(5,10)");
+}
