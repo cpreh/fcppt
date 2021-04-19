9481df235e17f884dbff0f3b6f528158f071e349
Carl Philipp Reh <carlphilippreh@gmail.com>
2021-04-19 09:49:04 +0200

    Replace gmtime and localtime implementations

--- libs/core/src/time/gmtime.cpp
+++ libs/core/src/time/gmtime.cpp
@@ -3,9 +3,32 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
+#include <fcppt/text.hpp>
+#include <fcppt/config/platform.hpp>
+#include <fcppt/time/exception.hpp>
 #include <fcppt/time/gmtime.hpp>
 #include <fcppt/config/external_begin.hpp>
 #include <ctime>
+#include <time.h> // NOLINT(hicpp-deprecated-headers,modernize-deprecated-headers)
 #include <fcppt/config/external_end.hpp>
 
-std::tm fcppt::time::gmtime(std::time_t const _time) { return *std::gmtime(&_time); }
+std::tm fcppt::time::gmtime(std::time_t const _time)
+{
+#if defined(FCPPT_CONFIG_POSIX_PLATFORM)
+  std::tm result{};
+  if (gmtime_r(&_time, &result) == nullptr)
+  {
+    throw fcppt::time::exception{FCPPT_TEXT("gmtime_r failed")};
+  }
+  return result;
+#elif defined(FCPPT_CONFIG_WINDOWS_PLATFORM)
+  std::tm result{};
+  if(gmtime_s(&result, &_time) != 0)
+  {
+    throw fcppt::time::exception{FCPPT_TEXT("gmtime_s failed")};
+  }
+  return result;
+#else
+#error "Implement me!"
+#endif
+}
--- libs/core/src/time/localtime.cpp
+++ libs/core/src/time/localtime.cpp
@@ -3,9 +3,32 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
+#include <fcppt/text.hpp>
+#include <fcppt/config/platform.hpp>
+#include <fcppt/time/exception.hpp>
 #include <fcppt/time/localtime.hpp>
 #include <fcppt/config/external_begin.hpp>
 #include <ctime>
+#include <time.h> // NOLINT(hicpp-deprecated-headers,modernize-deprecated-headers)
 #include <fcppt/config/external_end.hpp>
 
-std::tm fcppt::time::localtime(std::time_t const _time) { return *std::localtime(&_time); }
+std::tm fcppt::time::localtime(std::time_t const _time)
+{
+#if defined(FCPPT_CONFIG_POSIX_PLATFORM)
+  std::tm result{};
+  if (localtime_r(&_time, &result) == nullptr)
+  {
+    throw fcppt::time::exception{FCPPT_TEXT("localtime_r failed")};
+  }
+  return result;
+#elif defined(FCPPT_CONFIG_WINDOWS_PLATFORM)
+  std::tm result{};
+  if(localtime_s(&result, &_time) != 0)
+  {
+    throw fcppt::time::exception{FCPPT_TEXT("localtime_s failed")};
+  }
+  return result;
+#else
+#error "Implement me!"
+#endif
+}
