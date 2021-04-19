0f368c902d25b083529dc212fd4952192d0e6506
Carl Philipp Reh <carlphilippreh@gmail.com>
2021-04-19 15:15:25 +0200

    Ignore warning about getenv in VC++

--- libs/core/src/getenv.cpp
+++ libs/core/src/getenv.cpp
@@ -5,6 +5,9 @@
 
 #include <fcppt/getenv.hpp>
 #include <fcppt/optional_std_string.hpp>
+#include <fcppt/preprocessor/disable_vc_warning.hpp>
+#include <fcppt/preprocessor/pop_warning.hpp>
+#include <fcppt/preprocessor/push_warning.hpp>
 #include <fcppt/config/external_begin.hpp>
 #include <cstdlib>
 #include <string>
@@ -13,7 +16,10 @@
 
 fcppt::optional_std_string fcppt::getenv(std::string_view const &_val)
 {
+FCPPT_PP_PUSH_WARNING
+FCPPT_PP_DISABLE_VC_WARNING(4996)
   char const *const ret{std::getenv(std::string{_val}.c_str())};
+FCPPT_PP_POP_WARNING
 
   return ret == nullptr ? fcppt::optional_std_string{}
                         : fcppt::optional_std_string{std::string(ret)};
