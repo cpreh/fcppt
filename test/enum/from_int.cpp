7884509ec5c83f207879f2db0c814402018cf678
Carl Philipp Reh <carlphilippreh@gmail.com>
2021-04-21 10:15:47 +0200

    Removed enum::from_int_exn

--- test/enum/from_int.cpp
+++ test/enum/from_int.cpp
@@ -3,10 +3,8 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
-#include <fcppt/exception.hpp>
 #include <fcppt/catch/optional.hpp>
 #include <fcppt/enum/from_int.hpp>
-#include <fcppt/enum/from_int_exn.hpp>
 #include <fcppt/optional/make.hpp>
 #include <fcppt/optional/object.hpp>
 #include <fcppt/config/external_begin.hpp>
@@ -29,11 +27,4 @@ TEST_CASE("enum::from_int", "[enum]")
   CHECK(fcppt::enum_::from_int<test_enum>(1U) == fcppt::optional::make(test_enum::enum2));
 
   CHECK(fcppt::enum_::from_int<test_enum>(2U) == fcppt::optional::object<test_enum>{});
-}
-
-TEST_CASE("enum::from_int_exn", "[enum]")
-{
-  CHECK(fcppt::enum_::from_int_exn<test_enum>(1U) == test_enum::enum2);
-
-  CHECK_THROWS_AS(fcppt::enum_::from_int_exn<test_enum>(2U), fcppt::exception);
 }
