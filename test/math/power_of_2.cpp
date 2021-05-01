0ee074346543eda3e29887611f7de4997ddf0361
Carl Philipp Reh <carlphilippreh@gmail.com>
2021-05-01 09:29:38 +0200

    Added power_of_2 test

---
+++ test/math/power_of_2.cpp
@@ -0,0 +1,18 @@
+//          Copyright Carl Philipp Reh 2009 - 2021.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#include <fcppt/math/power_of_2.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <cstdint>
+#include <catch2/catch.hpp>
+#include <fcppt/config/external_end.hpp>
+
+TEST_CASE("math::power_of_2", "[math]")
+{
+  static_assert(fcppt::math::power_of_2<unsigned>(2U) == 4U);
+  CHECK(fcppt::math::power_of_2<unsigned>(2U) == 4U);
+  static_assert(fcppt::math::power_of_2<std::uint8_t>(2U) == std::uint8_t{4});
+  CHECK(fcppt::math::power_of_2<std::uint8_t>(2U) == std::uint8_t{4});
+}
