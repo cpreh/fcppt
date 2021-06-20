25030d46ac4ecdbbc1ac65d3ca99d3dc51fe89ad
Carl Philipp Reh <carlphilippreh@gmail.com>
2021-06-20 10:57:46 +0200

    Add VC++ workaround for list::distinct

--- libs/core/include/fcppt/mpl/list/distinct.hpp
+++ libs/core/include/fcppt/mpl/list/distinct.hpp
@@ -6,6 +6,7 @@
 #ifndef FCPPT_MPL_LIST_DISTINCT_HPP_INCLUDED
 #define FCPPT_MPL_LIST_DISTINCT_HPP_INCLUDED
 
+#include <fcppt/config/compiler.hpp>
 #include <fcppt/mpl/list/object.hpp>
 #include <fcppt/mpl/list/object_concept.hpp>
 #include <fcppt/preprocessor/disable_gcc_warning.hpp>
@@ -20,6 +21,39 @@ namespace fcppt::mpl::list
 {
 namespace detail
 {
+#if defined(FCPPT_CONFIG_MSVC_COMPILER)
+namespace distinct_impl
+{
+template <typename... Types>
+struct distinct;
+
+template <>
+struct distinct<>
+{
+  using type = std::true_type;
+};
+
+template <typename T>
+struct distinct<T>
+{
+  using type = std::true_type;
+};
+
+template <typename T, typename... Types>
+struct distinct<T, Types...>
+{
+  using type = std::conjunction<std::negation<std::is_same<T, Types...>>>;
+};
+}
+template<typename T>
+struct distinct;
+
+template <typename... Types>
+struct distinct<fcppt::mpl::list::object<Types...>>
+{
+  using type = typename distinct_impl::distinct<Types...>::type;
+};
+#else
 namespace distinct_impl
 {
 template<typename T>
@@ -51,6 +85,8 @@ struct distinct<fcppt::mpl::list::object<Types...>>
   using type = std::conjunction<
       std::is_convertible<distinct_impl::derived<Types...>, distinct_impl::base<Types>>...>;
 };
+#endif
+
 }
 
 template<fcppt::mpl::list::object_concept List>
