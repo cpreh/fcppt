2b5e00c461545e449e26fc286ae15c512fa9c5bd
Carl Philipp Reh <carlphilippreh@gmail.com>
2021-07-02 08:50:07 +0200

    More efficient mpl::list::map

--- libs/core/include/fcppt/mpl/list/map.hpp
+++ libs/core/include/fcppt/mpl/list/map.hpp
@@ -6,25 +6,26 @@
 #ifndef FCPPT_MPL_LIST_MAP_HPP_INCLUDED
 #define FCPPT_MPL_LIST_MAP_HPP_INCLUDED
 
-#include <fcppt/mpl/arg.hpp>
-#include <fcppt/mpl/bind.hpp>
 #include <fcppt/mpl/lambda.hpp>
 #include <fcppt/mpl/lambda_concept.hpp>
-#include <fcppt/mpl/list/fold.hpp>
 #include <fcppt/mpl/list/object.hpp>
 #include <fcppt/mpl/list/object_concept.hpp>
-#include <fcppt/mpl/list/push_back.hpp>
 
 namespace fcppt::mpl::list
 {
+namespace detail
+{
+template<typename List, typename F>
+struct map;
+
+template<typename... Types,template<typename...> class F>
+struct map<fcppt::mpl::list::object<Types...>,fcppt::mpl::lambda<F>>
+{
+  using type = fcppt::mpl::list::object<F<Types>...>;
+};
+}
 template <fcppt::mpl::list::object_concept List, fcppt::mpl::lambda_concept F>
-using map = fcppt::mpl::list::fold<
-    List,
-    fcppt::mpl::bind<
-        fcppt::mpl::lambda<fcppt::mpl::list::push_back>,
-        fcppt::mpl::arg<2>,
-        fcppt::mpl::bind<F, fcppt::mpl::arg<1>>>,
-    fcppt::mpl::list::object<>>;
+using map = typename fcppt::mpl::list::detail::map<List,F>::type;
 }
 
 #endif
