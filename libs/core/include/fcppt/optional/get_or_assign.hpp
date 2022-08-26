dfe5cdc0fe2dc68102daead68ad67a2e25c9fa30
Carl Philipp Reh <carlphilippreh@gmail.com>
2022-08-26 09:43:16 +0200

    Added optional::get_or_assign

---
+++ libs/core/include/fcppt/optional/get_or_assign.hpp
@@ -0,0 +1,38 @@
+//          Copyright Carl Philipp Reh 2009 - 2021.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef FCPPT_OPTIONAL_GET_OR_ASSIGN_HPP_INCLUDED
+#define FCPPT_OPTIONAL_GET_OR_ASSIGN_HPP_INCLUDED
+
+#include <fcppt/concepts/invocable_move.hpp>
+#include <fcppt/optional/object_impl.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <type_traits>
+#include <fcppt/config/external_end.hpp>
+
+namespace fcppt::optional
+{
+/**
+\brief Assigns an optional if it is empty. Returns a reference to the optional's contents.
+
+\ingroup fcpptoptional
+
+Assigns the result of \a _function to \a _optional and returns a reference to the contents of \a _optional.
+*/
+template <typename Element, fcppt::concepts::invocable_move<> Function>
+[[nodiscard]] inline Element &get_or_assign(
+    fcppt::optional::object<Element> &_optional,
+    Function const &_function) requires std::is_same_v<std::invoke_result_t<Function>, Element>
+{
+  if (!_optional.has_value())
+  {
+    _optional = fcppt::optional::object<Element>{_function()};
+  }
+
+  return _optional.get_unsafe();
+}
+}
+
+#endif
---
+++ test/optional/get_or_assign.cpp
@@ -0,0 +1,30 @@
+//          Copyright Carl Philipp Reh 2009 - 2021.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#include <fcppt/const.hpp>
+#include <fcppt/catch/begin.hpp>
+#include <fcppt/catch/end.hpp>
+#include <fcppt/optional/get_or_assign.hpp>
+#include <fcppt/optional/object_impl.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <fcppt/config/external_end.hpp>
+
+FCPPT_CATCH_BEGIN
+
+TEST_CASE("optional::get_or_assign", "[optional]")
+{
+  using optional_int = fcppt::optional::object<int>;
+
+  optional_int test{};
+
+  int const &result1{fcppt::optional::get_or_assign(test, fcppt::const_(1))};
+
+  CHECK(result1 == 1);
+
+  int const &result2{fcppt::optional::get_or_assign(test, fcppt::const_(2))};
+
+  CHECK(result2 == 1);
+}
