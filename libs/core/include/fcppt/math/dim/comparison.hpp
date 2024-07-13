e3f49717442e2481d2d0484e4c628a7d3f0ba01e
Carl Philipp Reh <carlphilippreh@gmail.com>
2024-07-13 10:00:51 +0200

    Remove ordering operators of dim

--- libs/core/include/fcppt/math/dim/comparison.hpp
+++ libs/core/include/fcppt/math/dim/comparison.hpp
@@ -8,7 +8,6 @@
 
 #include <fcppt/math/size_type.hpp>
 #include <fcppt/math/detail/array_equal.hpp>
-#include <fcppt/math/detail/array_less.hpp>
 #include <fcppt/math/dim/object_impl.hpp>
 
 namespace fcppt::math::dim
@@ -35,54 +34,6 @@ inline bool operator!=(
     fcppt::math::dim::object<T, N, S1> const &_v1, fcppt::math::dim::object<T, N, S2> const &_v2)
 {
   return !(_v1 == _v2);
-}
-
-/**
-\brief Compares two dims lexicographically
-
-\ingroup fcpptmathdim
-*/
-template <typename T, fcppt::math::size_type N, typename S1, typename S2>
-inline bool operator<(
-    fcppt::math::dim::object<T, N, S1> const &_v1, fcppt::math::dim::object<T, N, S2> const &_v2)
-{
-  return fcppt::math::detail::array_less(_v1, _v2);
-}
-
-/**
-\brief Compares two dims lexicographically
-
-\ingroup fcpptmathdim
-*/
-template <typename T, fcppt::math::size_type N, typename S1, typename S2>
-inline bool operator>(
-    fcppt::math::dim::object<T, N, S1> const &_v1, fcppt::math::dim::object<T, N, S2> const &_v2)
-{
-  return _v2 < _v1;
-}
-
-/**
-\brief Compares two dims lexicographically
-
-\ingroup fcpptmathdim
-*/
-template <typename T, fcppt::math::size_type N, typename S1, typename S2>
-inline bool operator<=(
-    fcppt::math::dim::object<T, N, S1> const &_v1, fcppt::math::dim::object<T, N, S2> const &_v2)
-{
-  return !(_v2 < _v1);
-}
-
-/**
-\brief Compares two dims lexicographically
-
-\ingroup fcpptmathdim
-*/
-template <typename T, fcppt::math::size_type N, typename S1, typename S2>
-inline bool operator>=(
-    fcppt::math::dim::object<T, N, S1> const &_v1, fcppt::math::dim::object<T, N, S2> const &_v2)
-{
-  return !(_v1 < _v2);
 }
 
 }
