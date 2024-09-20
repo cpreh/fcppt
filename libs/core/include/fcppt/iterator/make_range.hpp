5a670933fb3bd4a2bedbdb020e8b414e168930a8
Carl Philipp Reh <carlphilippreh@gmail.com>
2024-09-20 18:09:16 +0200

    Move arguments in iterator::make_range

--- libs/core/include/fcppt/iterator/make_range.hpp
+++ libs/core/include/fcppt/iterator/make_range.hpp
@@ -6,7 +6,11 @@
 #ifndef FCPPT_ITERATOR_MAKE_RANGE_HPP_INCLUDED
 #define FCPPT_ITERATOR_MAKE_RANGE_HPP_INCLUDED
 
+#include <fcppt/move_if_rvalue.hpp>
 #include <fcppt/iterator/range_impl.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <type_traits>
+#include <fcppt/config/external_end.hpp>
 
 namespace fcppt::iterator
 {
@@ -15,12 +19,14 @@ namespace fcppt::iterator
 
 \ingroup fcpptiterator
 */
-template <typename Iterator>
-inline fcppt::iterator::range<Iterator> make_range(Iterator const _begin, Iterator const _end)
+template <typename Iterator1, typename Iterator2>
+inline fcppt::iterator::range<std::remove_cvref_t<Iterator1>>
+make_range(Iterator1 &&_begin, Iterator2 &&_end) // NOLINT(cppcoreguidelines-missing-std-forward)
+  requires(std::is_same_v<std::remove_cvref_t<Iterator1>, std::remove_cvref_t<Iterator2>>)
 {
-  return fcppt::iterator::range<Iterator>(_begin, _end);
+  return fcppt::iterator::range<std::remove_cvref_t<Iterator1>>(
+      fcppt::move_if_rvalue<Iterator1>(_begin), fcppt::move_if_rvalue<Iterator2>(_end));
 }
-
 }
 
 #endif
