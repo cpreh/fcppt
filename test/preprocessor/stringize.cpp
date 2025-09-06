09c0c34140cb5ba1906b066e8938dd929658c689
Carl Philipp Reh <carlphilippreh@gmail.com>
2025-09-06 08:18:21 +0200

    Add PP_STRINGIZE test

---
+++ test/preprocessor/stringize.cpp
@@ -0,0 +1,24 @@
+//          Copyright Carl Philipp Reh 2009 - 2021.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#include <fcppt/string.hpp>
+#include <fcppt/text.hpp>
+#include <fcppt/catch/begin.hpp>
+#include <fcppt/catch/end.hpp>
+#include <fcppt/preprocessor/stringize.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <fcppt/config/external_end.hpp>
+
+FCPPT_CATCH_BEGIN
+// NOLINTBEGIN(clang-analyzer-optin.core.EnumCastOutOfRange,misc-const-correctness,cert-err58-cpp,fuchsia-statically-constructed-objects,misc-use-anonymous-namespace,cppcoreguidelines-avoid-do-while)
+
+TEST_CASE("preprocessor::stringize", "[preprocessor]")
+{
+  CHECK(fcppt::string{FCPPT_PP_STRINGIZE(test123)} == fcppt::string{FCPPT_TEXT("test123")});
+}
+
+// NOLINTEND(clang-analyzer-optin.core.EnumCastOutOfRange,misc-const-correctness,cert-err58-cpp,fuchsia-statically-constructed-objects,misc-use-anonymous-namespace,cppcoreguidelines-avoid-do-while)
+FCPPT_CATCH_END
