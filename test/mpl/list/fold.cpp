759a57d8e4d935e1ede8aeb959749c4ad12446d5
Carl Philipp Reh <carlphilippreh@gmail.com>
2026-01-31 20:44:51 +0100

    Use mpl::add in mpl/list/fold test

--- test/mpl/list/fold.cpp
+++ test/mpl/list/fold.cpp
@@ -3,6 +3,7 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
+#include <fcppt/mpl/add.hpp>
 #include <fcppt/mpl/lambda.hpp>
 #include <fcppt/mpl/list/fold.hpp>
 #include <fcppt/mpl/list/object.hpp>
@@ -14,18 +15,6 @@ namespace
 {
 template<unsigned N>
 using integral = std::integral_constant<unsigned,N>;
-
-// TODO(philipp): Put this into fcppt::mpl
-template<typename T, typename U>
-struct add_impl;
-
-template<typename T, T A, T B>
-struct add_impl<std::integral_constant<T,A>,std::integral_constant<T,B>>
-{
-  using type = std::integral_constant<T,A+B>;
-};
-template<typename T, typename U>
-using add =  typename add_impl<T,U>::type;
 }
 
 int main()
@@ -33,7 +22,7 @@ int main()
   static_assert(std::is_same_v<
                 fcppt::mpl::list::fold<
                     fcppt::mpl::list::object<integral<1U>, integral<2U>>,
-                    fcppt::mpl::lambda<add>,
+                    fcppt::mpl::lambda<fcppt::mpl::add>,
                     integral<0U>>,
                 integral<3U>>);
 }
