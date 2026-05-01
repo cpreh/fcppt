73a7618d38121c069b396e5500c74374aa4e6504
Carl Philipp Reh <carlphilippreh@gmail.com>
2026-05-01 13:11:03 +0200

    Use optional::return_if in is_flag

--- libs/options/impl/src/options/impl/is_flag.cpp
+++ libs/options/impl/src/options/impl/is_flag.cpp
@@ -10,6 +10,7 @@
 #include <fcppt/optional/bind.hpp>
 #include <fcppt/optional/map.hpp>
 #include <fcppt/optional/object_impl.hpp>
+#include <fcppt/optional/return_if.hpp>
 #include <fcppt/options/detail/flag_is_short.hpp>
 #include <fcppt/options/impl/is_flag.hpp>
 #include <fcppt/preprocessor/ignore_unsafe_buffer_usage.hpp>
@@ -27,30 +28,30 @@ FCPPT_PP_IGNORE_UNSAFE_BUFFER_USAGE
 fcppt::optional::object<std::pair<fcppt::options::detail::flag_is_short, fcppt::string_view>>
 fcppt::options::impl::is_flag(fcppt::string_view const _value)
 {
-  using result_type =
-      fcppt::optional::object<std::pair<fcppt::options::detail::flag_is_short, fcppt::string_view>>;
-
   auto const is_dash{[](fcppt::char_type const _ch) { return _ch == FCPPT_TEXT('-'); }};
 
   return fcppt::optional::bind(
       fcppt::container::uncons_string_view(_value),
       [is_dash](fcppt::tuple::object<fcppt::char_type, fcppt::string_view> const _pos1)
       {
-        fcppt::string_view const rest_view{fcppt::tuple::get<1>(_pos1)};
+        return fcppt::optional::return_if(
+            is_dash(fcppt::tuple::get<0>(_pos1)),
+            [is_dash, _pos1]
+            {
+              fcppt::string_view const rest_view{fcppt::tuple::get<1>(_pos1)};
 
-        return is_dash(fcppt::tuple::get<0>(_pos1))
-                   ? fcppt::optional::map(
-                         fcppt::container::uncons_string_view(rest_view),
-                         [is_dash, rest_view](
-                             fcppt::tuple::object<fcppt::char_type, fcppt::string_view> const _pos2)
-                         {
-                           bool const dash_here{is_dash(fcppt::tuple::get<0>(_pos2))};
+              return fcppt::optional::map(
+                  fcppt::container::uncons_string_view(rest_view),
+                  [is_dash, rest_view](
+                      fcppt::tuple::object<fcppt::char_type, fcppt::string_view> const _pos2)
+                  {
+                    bool const dash_here{is_dash(fcppt::tuple::get<0>(_pos2))};
 
-                           return std::make_pair(
-                               fcppt::options::detail::flag_is_short{!dash_here},
-                               dash_here ? fcppt::tuple::get<1>(_pos2) : rest_view);
-                         })
-                   : result_type{};
+                    return std::make_pair(
+                        fcppt::options::detail::flag_is_short{!dash_here},
+                        dash_here ? fcppt::tuple::get<1>(_pos2) : rest_view);
+                  });
+            });
       });
 }
 
